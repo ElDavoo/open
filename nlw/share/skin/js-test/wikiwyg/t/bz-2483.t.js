542559b0023c794a2f4b9a39d693fd276eb55366
Audrey Tang <audrey.tang@socialtext.com>
2009-07-27 21:22:35 -0700

    * Adjust js-test as IE does not recognize &apos;.  Also add a test on &nbsp; alone.

--- nlw/share/skin/js-test/wikiwyg/t/bz-2483.t.js
+++ nlw/share/skin/js-test/wikiwyg/t/bz-2483.t.js
@@ -4,14 +4,20 @@ t.filters({
     html: ['html_to_wikitext']
 });
 
-t.plan(1);
+t.plan(2);
 t.run_is('html', 'text');
 
 /* Test
 === IE: &nbsp incorrectly parsed as literal "nbsp" when inside anchor text
 --- html
-<div class="wiki"><a href="http://example.com/">Foo &nbsp; Bar &apos; Baz</a></div>
+<div class="wiki"><a href="http://example.com/">Foo &nbsp; Bar &#39; Baz</a></div>
 --- text
 "Foo Bar ' Baz"<http://example.com/>
+
+=== IE: &nbsp incorrectly parsed as literal "nbsp" when inside anchor text
+--- html
+<div class="wiki"><a href="http://example.com/">Foo Bar&nbsp;Baz</a></div>
+--- text
+"Foo Bar Baz"<http://example.com/>
 
 */
