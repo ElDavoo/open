6b700c48bec9eb4447cbcc610e4fae1642b0af56
Stash <jeremy.stashewsky@socialtext.com>
2009-07-31 15:56:13 -0700

    Clean up WorkspceUsers (no functional chg.)
    I think...  might need to bring back _entity_hash at some point.

--- nlw/lib/Socialtext/Rest/WorkspaceUsers.pm
+++ nlw/lib/Socialtext/Rest/WorkspaceUsers.pm
@@ -9,22 +9,13 @@ use Socialtext::JSON;
 use Socialtext::HTTP ':codes';
 use Socialtext::WorkspaceInvitation;
 use Socialtext::User::Find::Workspace;
+use Class::Field qw/field/;
 
-# FIXME: POST is not yet implemented
-#sub allowed_methods {'GET, HEAD, POST'}
 sub allowed_methods {'GET, HEAD, POST'}
 sub collection_name { "Users in workspace " . $_[0]->ws }
 
-sub _entity_hash {
-    my ( $self, $user ) = @_;
-    my $name  = $user->username;
-    my $email = $user->email_address;
-    return +{
-        name               => $name,
-        email              => $email,
-        uri                => "/data/users/$name",
-    };
-}
+field '_workspace',
+    '-init' => 'Socialtext::Workspace->new(name => $self->ws)';
 
 sub create_user_find {
     my $self = shift;
@@ -34,7 +25,7 @@ sub create_user_find {
     my $offset = $self->rest->query->param('offset') || 0;
 
     my $filter = $self->rest->query->param('filter');
-    my $workspace = Socialtext::Workspace->new( name => $self->ws );
+    my $workspace = $self->_workspace;
     die "invalid workspace" unless $workspace;
 
     return Socialtext::User::Find::Workspace->new(
@@ -52,27 +43,18 @@ sub if_authorized {
     my $call = shift;
 
     my $acting_user = $self->rest->user;
+    my $checker = $self->hub->checker;
 
     if ($method eq 'POST') {
         return $self->not_authorized 
-            unless $self->rest->user->is_business_admin()
-                || $self->rest->user->is_technical_admin()
-                || $self->hub->checker->check_permission('admin_workspace');
+            unless $acting_user->is_business_admin()
+                || $acting_user->is_technical_admin()
+                || $checker->check_permission('admin_workspace');
     }
     elsif ($method eq 'GET') {
-        my $can_admin = $self->workspace->permissions->user_can(
-            user       => $acting_user,
-            permission =>
-                Socialtext::Permission->new( name => 'admin_workspace' ),
-        );
-        # REVIEW: {bz: 1265} requires everybody with "Email this Page"
-        # permission to see the workspace user list.  Is this desired?
-        my $can_email = $self->workspace->permissions->user_can(
-            user       => $acting_user,
-            permission =>
-                Socialtext::Permission->new( name => 'email_out' ),
-        );
-        return $self->not_authorized unless $can_admin or $can_email;
+        return $self->not_authorized
+            unless $checker->check_permission('admin_workspace')
+                || $checker->check_permission('email_out');
     }
     else {
         return $self->bad_method;
@@ -90,6 +72,9 @@ sub _POST {
     my $self = shift;
     my $rest = shift;
 
+    my $workspace = $self->_workspace
+        || return $self->no_workspace();
+
     my $create_request_hash = decode_json( $rest->getContent() );
 
     unless ( $create_request_hash->{username} and
@@ -103,12 +88,6 @@ sub _POST {
     my $workspace_name = $self->ws;
     my $username = $create_request_hash->{username};
     my $rolename = $create_request_hash->{rolename};
-
-    my $workspace = Socialtext::Workspace->new( name => $workspace_name );
-    
-    unless( $workspace ) {
-        return $self->no_workspace();
-    }
 
     eval {
         if ( $create_request_hash->{send_confirmation_invitation} ) {
