9ab802b8b9eaac4917d235e53d05d35fc3fc9f68
Matthew Heusser <matt.heusser@socialtext.com>
2009-08-06 13:13:59 -0700

    Text for 2 widgets changed plus framework tweak
    (I changed framework code to enable parsing of logs on an appliance)

--- nlw/lib/Socialtext/WikiFixture/Socialtext.pm
+++ nlw/lib/Socialtext/WikiFixture/Socialtext.pm
@@ -667,12 +667,12 @@ sub st_catchup_logs {
        my $new_dir =  $ENV{ST_CURRENT} . "/socialtext-reports/";
        chdir($new_dir);
        my $str = $ENV{ST_CURRENT} . "/socialtext-reports/parse-dev-env-logs /var/log/nlw.log >/dev/null 2>&1";
-       shell_run($str);
+       _run_command($str);
        chdir($current_dir);
    } else {
       #On An Appliance
-      shell_run("sudo /usr/bin/st-reports-consume-access-log /var/log/apache-perl/access.log >> /var/log/st-reports.log >/dev/null 2>&1");
-      shell_run("sudo /usr/bin/st-reports-consume-nlw-log /var/log/nlw.log >> /var/log/st-reports.log >/dev/null 2>&1");
+      _run_command("sudo /usr/bin/st-reports-consume-access-log /var/log/apache-perl/access.log >> /var/log/st-reports.log >/dev/null 2>&1");
+      _run_command("sudo /usr/bin/st-reports-consume-nlw-log /var/log/nlw.log >> /var/log/st-reports.log >/dev/null 2>&1");
    }
 }
 
