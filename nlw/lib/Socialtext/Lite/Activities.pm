3ad2deb98472d6bc8beb3807c0323d72ca972205
Luke Closs <gravatar@5thplane.com>
2009-08-10 17:46:03 -0700

    fix up the activities tab in miki

--- nlw/lib/Socialtext/Lite/Activities.pm
+++ nlw/lib/Socialtext/Lite/Activities.pm
@@ -10,7 +10,7 @@ extends 'Socialtext::Lite';
 sub activities {
     my $self = shift;
     return $self->events(
-        event_class => '!view',
+        'action!' => 'view,edit_start,edit_cancel,watch_add,watch_delete',
         title => 'Activities',
         section => 'activities',
         @_,
@@ -22,21 +22,30 @@ sub events {
     my $viewer = $self->hub->current_user;
     my $page_size = 10;
 
-    my $events;
     my %event_args = (
-        event_class => $args{event_class},
+        ($args{event_class} ? (event_class => $args{event_class}) : ()),
         offset => $args{pagenum} * $page_size,
         count => $page_size + 1,
     );
     my $reporter = Socialtext::Events::Reporter->new(viewer => $viewer);
+
+    my $events;
     if ($args{mine}) {
         $events = $reporter->get_events_activities($viewer, \%event_args);
     }
+    elsif ($args{followed}) {
+        $events = $reporter->get_events_followed(\%event_args);
+    }
     elsif ($args{all}) {
-        $events = $reporter->get_events(\%event_args);
+        if ($args{section} eq 'activities') {
+            $events = $reporter->get_events_activities($viewer, \%event_args);
+        }
+        else {
+            $events = $reporter->get_events(\%event_args);
+        }
     }
     else {
-        $events = $reporter->get_events_followed(\%event_args);
+        $events = $reporter->get_events_conversations($viewer, \%event_args);
     }
     $events ||= [];
 
@@ -44,7 +53,7 @@ sub events {
 
     $self->hub->viewer->link_dictionary($self->link_dictionary);
     return $self->_process_template(
-        'lite/activities.html',
+        "lite/activities.html",
         events   => $events,
         more     => $more ? 1 : 0,
         base_uri => "/m/$args{section}",
