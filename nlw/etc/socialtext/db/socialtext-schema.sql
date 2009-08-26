a04d0edbcb66bd309ab6477c6560b371c6978088
Brandon Noard <brandon.noard@socialtext.com>
2009-08-26 14:29:26 -0700

    Move this migration to make way for a merge from 07-31.

--- nlw/etc/socialtext/db/socialtext-schema.sql
+++ nlw/etc/socialtext/db/socialtext-schema.sql
@@ -1845,4 +1845,4 @@ ALTER TABLE ONLY workspace_plugin
             REFERENCES "Workspace"(workspace_id) ON DELETE CASCADE;
 
 DELETE FROM "System" WHERE field = 'socialtext-schema-version';
-INSERT INTO "System" VALUES ('socialtext-schema-version', '83');
+INSERT INTO "System" VALUES ('socialtext-schema-version', '84');
