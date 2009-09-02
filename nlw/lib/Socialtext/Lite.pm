2c9fa2583859272459fa77cb6803868e29224693
Kevin Jones <kevin.jones@socialtext.com>
2009-09-02 09:15:02 -0700

    Alan had success with 4.5, so let's support 4.5

--- nlw/lib/Socialtext/Lite.pm
+++ nlw/lib/Socialtext/Lite.pm
@@ -426,8 +426,8 @@ sub _process_template {
     my $warning;
     my $ua = $ENV{HTTP_USER_AGENT};
     if (my ($version) = $ua =~ m{^BlackBerry[^/]+/(\S+) }) {
-        if ($version < 4.6) {
-            $warning = loc("Warning: Socialtext Mobile for BlackBerry requires v4.6 or higher");
+        if ($version < 4.5) {
+            $warning = loc("Warning: Socialtext Mobile requires BlackBerry OS v4.5 or higher");
         }
     }
 
