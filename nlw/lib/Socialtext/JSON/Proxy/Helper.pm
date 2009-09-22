43478843fec00366b6fa0c3040cdbe27a6349c96
Kevin Jones <kevin.jones@socialtext.com>
2009-09-22 16:04:55 -0700

    Make Proxy::Helper clear the disk/mem cache now.

--- nlw/lib/Socialtext/JSON/Proxy/Helper.pm
+++ nlw/lib/Socialtext/JSON/Proxy/Helper.pm
@@ -3,20 +3,36 @@ package Socialtext::JSON::Proxy::Helper;
 use strict;
 use warnings;
 
-use Socialtext::SQL qw(sql_execute);
+use File::Temp qw/mkdtemp/;
+use Socialtext::Paths;
+use Socialtext::AppConfig;
 
 sub ClearForUser {
-    my ($class, $user_id) = @_;
-    sql_execute('DELETE FROM json_proxy_cache WHERE user_id = ?', $user_id);
+    my $class = shift;
+    my $user_id = shift;
+    PurgeCache();
 }
 
 sub ClearForAccount {
-    my ($class, $account_id) = @_;
-    sql_execute('
-        DELETE FROM json_proxy_cache WHERE user_id IN (
-            SELECT user_id FROM account_user WHERE account_id = ?
-        )
-    ', $account_id);
+    my $class = shift;
+    my $account_id = shift;
+    PurgeCache();
+}
+
+sub PurgeCache {
+    my $class = shift;
+
+    my $pidfile = Socialtext::AppConfig->pid_file_dir . "/json-proxy.pid";
+    system "start-stop-daemon --stop --quiet --oknodo --pidfile $pidfile --signal USR1";
+
+    my $cache_dir = Socialtext::Paths::storage_directory('json_cache');
+    if (-d $cache_dir) {
+        my $tmp_dir = mkdtemp("$cache_dir.purge.XXXXXX");
+        rename $cache_dir => $tmp_dir
+            or die "can't rename cache dir to $tmp_dir: $!";
+    }
+
+    return $cache_dir;
 }
 
 1;
@@ -29,10 +45,22 @@ Socialtext::JSON::Proxy::Helper
 
 =head1 SYNOPSIS
 
-Socialtext::JSON::Proxy::Helper->clear_for_user($user_id)
+  Socialtext::JSON::Proxy::Helper->PurgeCache
+  # Deprecated:
+  Socialtext::JSON::Proxy::Helper->ClearForUser($user_id);
+  Socialtext::JSON::Proxy::Helper->ClearForAccount($account_id);
 
 =head1 DESCRIPTION
 
-Socialtext::JSON::Proxy uses Mouse. Therefore, within NLW, this module should be used for holding all utility functions for accessing or clearing the JSON Proxy.
+Contains code that is directly callable by the main NLW app for modifying the JSON Proxy Cache.
+
+=head1 METHODS
+
+=head2 PurgeCache
+
+Purge the entire JSON Proxy Cache.  Will create directories of the form
+"json_cache.purge.XXXXXX" that will need to be cleaned up by some periodic
+process (e.g. cron).  C<st-remove-expired-proxy-cache> will do this on
+appliances.
 
 =cut
