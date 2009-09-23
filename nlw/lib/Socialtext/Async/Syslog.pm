6fce01430d6e234205b2568973b5d8f85388fa0d
Kevin Jones <kevin.jones@socialtext.com>
2009-09-23 13:40:54 -0700

    Fix async syslog writing
    This previously worked fine in dev-envs, but nothing was showing up in
    nlw.log on appliances because the socket to syslog was being closed too
    early.

--- nlw/lib/Socialtext/Async/Syslog.pm
+++ nlw/lib/Socialtext/Async/Syslog.pm
@@ -22,39 +22,91 @@ per-process message ordering.
 =cut
 
 use Coro;
+use AnyEvent;
+use Coro::AnyEvent ();
 use Coro::AIO;
-use Coro::Channel;
+use Coro::Semaphore;
 use Sys::Syslog ();
+use Guard;
+use Fcntl;
+use Log::Dispatch::Syslog ();
+use Log::Dispatch::File::Socialtext ();
 
 unless ($] eq '5.008007' || $Sys::Syslog::VERSION eq '0.09') {
     warn "CAUTION: ".__PACKAGE__." may not work with your version of perl!\n";
     warn "CAUTION: Please inspect how Sys::Syslog works to see if it's changed.\n";
 }
 
+no warnings 'redefine';
+*Sys::Syslog::_syslog_send_socket = \&send_to_syslog_aio;
+*Sys::Syslog::_syslog_send_stream = \&send_to_syslog_aio;
+
+sub send_to_syslog_aio {
+    my $buf = shift;
+    Coro::AnyEvent::writable $Sys::Syslog::{SYSLOG};
+    return syswrite $Sys::Syslog::{SYSLOG}, $buf, length($buf);
+}
+
+
+my $orig_log_message = \&Log::Dispatch::Syslog::log_message;
 {
     no warnings 'redefine';
-    *Sys::Syslog::_syslog_send_socket = \&send_to_syslog_aio;
-    *Sys::Syslog::_syslog_send_stream = \&send_to_syslog_aio;
+    *Log::Dispatch::Syslog::log_message = \&unblocked_syslog_message;
 }
 
 {
-    my $log_q = Coro::Channel->new;
-    sub send_to_syslog_aio {
-        my $buf = shift;
-        $log_q->put(\$buf);
-        return length($buf); # pretend that it worked.
-    }
-
-    async {
-        $Coro::current->{desc} = __PACKAGE__.' writer';
-        while (1) {
-            my $buf = $log_q->get;
-            return unless $buf;
-            eval {
-                aio_write(\*Sys::Syslog::SYSLOG, 0, length($$buf), $$buf, 0);
-            };
+    my $mutex = Coro::Semaphore->new;
+    sub unblocked_syslog_message {
+        my @args = @_;
+        async_pool {
+            my $g;
+            $g = $mutex->guard;
+            $orig_log_message->(@args);
         }
-    };
+    }
+}
+
+{
+    no warnings 'redefine';
+    *Log::Dispatch::File::Socialtext::_open_file = \&dont_open_file;
+    *Log::Dispatch::File::Socialtext::log_message = \&aio_write_to_file;
+}
+
+sub dont_open_file {
+    my $self = shift;
+    $self->{fh} = undef;
+}
+
+{
+    my $mutex = Coro::Semaphore->new;
+    sub aio_write_to_file {
+        my $self = shift;
+        my %p = @_;
+        async_pool {
+            $self->{fh} = undef;
+            my $g = $mutex->guard;
+
+            my $flags = O_APPEND|O_CREAT|O_WRONLY;
+            my $mode = $self->{permissions} || 0660;
+            my $fh;
+            $fh = aio_open($self->{filename}, $flags, $mode)
+                or die "cannot open $self->{filename}: $!";
+            scope_guard {aio_close $fh};
+
+            if ($self->{permissions} && !$self->{chmodded}) {
+                my $st = aio_chmod $self->{filename}, $self->{permissions};
+                die "Cannot chmod $self->{filename} to $self->{permissions}: $!"
+                    if ($st < 0);
+                $self->{chmodded} = 1;
+            }
+
+            my $msg = $p{message};
+            $msg = Encode::encode_utf8($msg) if Encode::is_utf8($msg);
+
+            my $wrote = aio_write($fh, 0, length($msg), $msg, 0);
+            die "Cannot syswrite to '$self->{filename}': $!" if $wrote <=0
+        };
+    }
 }
 
 1;
