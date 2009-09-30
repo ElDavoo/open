300881e93e67e2e92ceca38e5a3c93f49ef35826
Luke Closs <gravatar@5thplane.com>
2009-09-29 17:25:14 -0700

    Add /data/groups/:group_id/users code & tests

---
+++ nlw/lib/Socialtext/Rest/Group/Users.pm
@@ -0,0 +1,34 @@
+package Socialtext::Rest::Group::Users;
+# @COPYRIGHT@
+use Moose;
+extends 'Socialtext::Rest::Groups';
+use Socialtext::Group;
+use namespace::clean -except => 'meta';
+
+# Anybody can see these, since they are just the list of workspaces the user
+# has 'selected'.
+sub permission { +{} }
+
+sub collection_name { 'Group users' }
+
+sub _entities_for_query {
+    my $self = shift;
+    my $group_id = $self->group_id;
+    my $user = $self->rest->user();
+
+    my $group = Socialtext::Group->GetGroup(group_id => $group_id);
+    die Socialtext::Exception::NotFound->new() unless $group;
+
+    die Socialtext::Exception::NotFound->new()
+        unless $user->is_business_admin
+            or $group->creator->user_id == $user->user_id
+            or $group->has_user($user);
+
+    my $users = $group->users_as_minimal_arrayref();
+    return @$users;
+}
+
+sub _entity_hash { return $_[1] }
+
+__PACKAGE__->meta->make_immutable(inline_constructor => 0);
+1;
