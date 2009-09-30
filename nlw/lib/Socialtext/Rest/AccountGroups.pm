35f1f892ee9c192ca449b3842fb1a508c058f095
Luke Closs <gravatar@5thplane.com>
2009-09-30 16:26:46 -0700

    Add POST to /data/accounts/:acct/groups

--- nlw/lib/Socialtext/Rest/AccountGroups.pm
+++ nlw/lib/Socialtext/Rest/AccountGroups.pm
@@ -2,20 +2,23 @@ package Socialtext::Rest::AccountGroups;
 # @COPYRIGHT@
 use Moose;
 use Socialtext::Account;
+use Socialtext::HTTP ':codes';
+use Socialtext::JSON qw/decode_json/;
 use namespace::clean -except => 'meta';
 
 extends 'Socialtext::Rest::Collection';
 
-sub permission { +{ GET => undef } }
+has 'account' => (is => 'ro', isa => 'Maybe[Object]', lazy_build => 1);
+
+sub permission { +{ GET => undef, POST => undef } }
+sub allowed_methods { 'POST', 'GET' }
 sub collection_name { "Account Groups" }
 
 sub _entities_for_query {
     my $self      = shift;
     my $rest      = $self->rest;
     my $user      = $rest->user;
-    my $acct_name = $self->acct();
-    my $account   = Socialtext::Account->new( name => $acct_name );
-    return () unless $account;
+    my $account   = $self->account or return ();
 
     my @groups;
     my $group_cursor = $account->groups();
@@ -37,6 +40,14 @@ sub _entities_for_query {
     return @groups;
 }
 
+sub _build_account {
+    my $self = shift;
+
+    return Socialtext::Account->new( 
+        name => Socialtext::String::uri_unescape( $self->acct ),
+    );
+}
+
 sub _entity_hash {
     my $self  = shift;
     my $group = shift;
@@ -52,6 +63,72 @@ around get_resource => sub {
     return $orig->($self, @_);
 };
 
+sub POST_json {
+    my $self = shift;
+    my $rest = shift;
+    my $data = decode_json( $rest->getContent() );
+
+    unless ($self->user_can('is_business_admin')) {
+        $rest->header(
+            -status => HTTP_401_Unauthorized,
+        );
+        return '';
+    }
+
+    my $account = $self->account;
+    unless ( defined $account ) {
+        $rest->header(
+            -status => HTTP_404_Not_Found,
+        );
+        return '';
+    }
+
+    unless ( defined $data and ref($data) eq 'HASH' ) {
+        $rest->header(
+            -status => HTTP_400_Bad_Request,
+        );
+        return '';
+    }
+
+    my $group_id = $data->{group_id};
+    unless ($group_id) {
+        $rest->header(
+            -status => HTTP_400_Bad_Request,
+        );
+        return "Missing a group_id";
+    }
+
+    my $group = Socialtext::Group->GetGroup(group_id => $group_id);
+    unless ($group) {
+        $rest->header(
+            -status => HTTP_400_Bad_Request,
+        );
+        return "Group_id ($group_id) is not a valid group";
+    }
+
+    my $role;
+    if (my $role_name = $data->{role_name}) {
+        $role = Socialtext::Role->new(name => $role_name);
+    }
+
+    if ($account->has_group($group)) {
+        $rest->header(
+            -status => HTTP_409_Conflict,
+        );
+        return "Group_id ($group_id) is already in this account.";
+    }
+
+    $account->add_group(
+        group => $group,
+        ($role ? (role => $role) : ()),
+    );
+
+    $rest->header(
+        -status => HTTP_204_No_Content,
+    );
+    return '';
+}
+
 __PACKAGE__->meta->make_immutable( inline_constructor => 0 );
 1;
 
@@ -62,6 +139,9 @@ Socialtext::Rest::AccountGroups - Groups in an account.
 =head1 SYNOPSIS
 
     GET /data/accounts/:acct/groups
+
+    POST /data/accounts/:acct/groups as application/json
+    - Body should be a JSON hash containing a group_id and optionally a role_name.
 
 =head1 DESCRIPTION
 
