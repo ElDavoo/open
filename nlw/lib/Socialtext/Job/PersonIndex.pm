31e2a45b0bc3e204bf7118badb312e278c1dd247
Luke Closs <gravatar@5thplane.com>
2009-10-05 16:55:21 -0700

    Add the missing PersonIndex job code

---
+++ nlw/lib/Socialtext/Job/PersonIndex.pm
@@ -0,0 +1,34 @@
+package Socialtext::Job::PersonIndex;
+# @COPYRIGHT@
+use Moose;
+use namespace::clean -except => 'meta';
+
+extends 'Socialtext::Job';
+
+sub do_work {
+    my $self    = shift;
+    my $indexer = $self->indexer or return;
+
+    $indexer->index_person($self->user);
+
+    $self->completed();
+}
+
+__PACKAGE__->meta->make_immutable;
+1;
+
+=head1 NAME
+
+Socialtext::Job::PersonIndex - index a person profile.
+
+=head1 SYNOPSIS
+
+    use Socialtext::JobCreator;
+
+    Socialtext::JobCreator->index_person($user);
+
+=head1 DESCRIPTION
+
+Schedule a job to be run by TheCeq which will index the profile using Solr.
+
+=cut
