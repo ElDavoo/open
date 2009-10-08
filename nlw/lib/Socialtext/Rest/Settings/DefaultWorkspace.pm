769e9dc50e9c996af552c861158064581102f3bd
Gabe Wachob <gwachob@wachob.com>
2009-10-08 16:50:52 -0700

    Handle the (very common) case of no default workspace by generating a 404, but not a system error in the log (Fer cryin' out LOUD!)

--- nlw/lib/Socialtext/Rest/Settings/DefaultWorkspace.pm
+++ nlw/lib/Socialtext/Rest/Settings/DefaultWorkspace.pm
@@ -9,6 +9,25 @@ use namespace::clean -except => 'meta';
 
 extends 'Socialtext::Rest::Workspace';
 
+sub _my_GET_any {
+    my( $self, $rest, $command ) = @_;
+    if ($self->workspace->real == 0) {
+        $rest->header(
+            -status => HTTP_404_Not_Found,
+        );
+        return "Default Workspace not found.";
+    }
+
+    # CopyPasta from ST::Rest:Workspace->_GET_any
+    my $super_method = (caller 1)[3];   # - get fully-qualified name of calling sub
+    $super_method =~ s/^.+::/SUPER::/;  # - replace package name with SUPER
+    return $self->$super_method($rest); # - call the superclass method
+}
+
+sub GET_html { _my_GET_any(@_) }
+sub GET_text { _my_GET_any(@_) }
+sub GET_json { _my_GET_any(@_) }
+
 sub _new_workspace {
     my $self = shift;
     return ( $self->ws )
@@ -19,12 +38,6 @@ sub _new_workspace {
 sub ws { 
     my ($self, $rest) = shift;
     my $name = Socialtext::AppConfig->default_workspace;
-
-    Socialtext::Exception->throw(
-        error => "Default Workspace not found.",
-        http_status => HTTP_404_Not_Found
-    ) unless $name;
-
     return $name;
 }
 
