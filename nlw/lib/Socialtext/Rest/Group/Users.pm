b0a2e156f8939251f6384b2ba0fa77318f5a1008
Brandon Noard <brandon.noard@socialtext.com>
2009-10-28 15:29:09 -0700

    Add User to Group via ReST.

--- nlw/lib/Socialtext/Rest/Group/Users.pm
+++ nlw/lib/Socialtext/Rest/Group/Users.pm
@@ -3,6 +3,9 @@ package Socialtext::Rest::Group::Users;
 use Moose;
 extends 'Socialtext::Rest::Groups';
 use Socialtext::Group;
+use Socialtext::HTTP ':codes';
+use Socialtext::JSON qw/decode_json/;
+use Socialtext::User;
 use namespace::clean -except => 'meta';
 
 # Anybody can see these, since they are just the list of workspaces the user
@@ -29,6 +32,63 @@ sub _entities_for_query {
 }
 
 sub _entity_hash { return $_[1] }
+
+sub POST_json {
+    my $self = shift;
+    my $rest = shift;
+    my $data = decode_json( $rest->getContent() );
+
+    # Only a Business Admin has permission to do this right now.
+    unless ($self->user_can('is_business_admin')) {
+        $rest->header( -status => HTTP_401_Unauthorized );
+        return '';
+    }
+
+    unless ( defined $data and ref($data) eq 'HASH' ) {
+        $rest->header( -status => HTTP_400_Bad_Request );
+        return '';
+    }
+
+    my $group = Socialtext::Group->GetGroup( group_id => $self->group_id );
+    die Socialtext::Exception::NotFound->new() unless $group;
+
+    # Group is not Socialtext sourced, we don't control its membership.
+    unless ( $group->can_update_store ) {
+        $rest->header( -status => HTTP_400_Bad_Request );
+        return '';
+    }
+
+    my $username = $data->{username};
+    unless ( $username ) {
+        $rest->header( -status => HTTP_400_Bad_Request );
+        return 'Missing a username';
+    }
+
+    my $user = Socialtext::User->new( username => $username );
+    unless ( $user ) {
+        $rest->header( -status => HTTP_400_Bad_Request );
+        return "User with $username does not exist";
+    }
+
+    # Note: We only have 'member' roles for Groups for now.
+    my $role_name = $data->{role_name} || 'member';
+    my $role = Socialtext::Role->new( name => $role_name );
+    unless ( $role && $role->name eq 'member' ) {
+        $rest->header( -status => HTTP_400_Bad_Request );
+        return "Invalid role name $role_name";
+    }
+
+    my $role_for_user = $group->role_for_user( user => $user );
+    if ($role_for_user && $role_for_user->name eq $role_name) {
+        $rest->header( -status => HTTP_400_Bad_Request );
+        return "User $username already has Role $role_name";
+    }
+
+    $group->add_user( user => $user, role => $role );
+
+    $rest->header( -status => HTTP_201_Created );
+    return '';
+}
 
 __PACKAGE__->meta->make_immutable(inline_constructor => 0);
 1;
