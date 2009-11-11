7761c07ae2788bccd5944858d19d0169f54d0dbe
Kevin Jones <kevin.jones@socialtext.com>
2009-11-10 16:59:51 -0800

    Add a grep option for lookaheads {bz: 2498}

--- nlw/share/skin/s3/javascript/lookahead.js
+++ nlw/share/skin/s3/javascript/lookahead.js
@@ -229,6 +229,8 @@
 
             var title = self.linkTitle(this);
             if (title.match(re)) {
+                if (self.opts.grep && !self.opts.grep(this)) return;
+
                 filtered.push({
                     bolded_title: title.replace(re, '<b>$1</b>'),
                     title: title,
