7192fedd833a928aa74c8bb8149385d07bf93ec5
Luke Closs <gravatar@5thplane.com>
2009-11-16 11:36:14 -0800

    Add st-admin rebuild-pagelinks - {bz: 3305}

--- nlw/lib/Socialtext/CLI.pm
+++ nlw/lib/Socialtext/CLI.pm
@@ -2604,6 +2604,26 @@ sub set_customjs {
     }
 }
 
+sub rebuild_pagelinks {
+    my $self = shift;
+
+    my %opts = $self->_get_options( 'workspace:s' );
+    $self->_error('You must specify a workspace')
+        if (!$opts{workspace});
+    my $workspace = $self->_load_workspace($opts{workspace});
+
+    Socialtext::JobCreator->insert(
+        'Socialtext::Job::Upgrade::RebuildPageLinks',
+        {
+            workspace_id => $workspace->workspace_id,
+        },
+    );
+
+    my $ws_name = $workspace->name;
+    $self->_success("A job has been created to rebuild page links for the "
+        . "$ws_name workspace.");
+}
+
 sub invite_user {
     my $self = shift;
 
@@ -3321,6 +3341,7 @@ Socialtext::CLI - Provides the implementation for the st-admin CLI script
   customjs --workspace
   set-customjs --workspace [--uri or --name]
   clear-customjs --workspace
+  rebuild-pagelinks --workspace
 
   INDEXING
 
@@ -3663,6 +3684,10 @@ Set the URI or name for the custom Javascript for a workspace.
 =head2 customjs --workspace
 
 Show the URI or name for the custom Javascript assigned to a workspace.
+
+=head2 rebuild-pagelinks --workspace
+
+Re-create the backlinks for the specified workspace.
 
 =head2 html-archive --workspace --file
 
