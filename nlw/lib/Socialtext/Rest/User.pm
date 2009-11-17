7963bfe95486ef7add064b63285d0715db555501
Luke Closs <gravatar@5thplane.com>
2009-11-16 17:20:38 -0800

    Add code + tests for PUT /data/users/:username to change primary account

--- nlw/lib/Socialtext/Rest/User.pm
+++ nlw/lib/Socialtext/Rest/User.pm
@@ -1,20 +1,17 @@
 package Socialtext::Rest::User;
 # @COPYRIGHT@
-
 use warnings;
 use strict;
-
 use base 'Socialtext::Rest::Entity';
-
 use Socialtext::Functional 'hgrep';
 use Socialtext::User;
-
-# FIXME: Attention paid to permissions is incomplete.
+use Socialtext::HTTP ':codes';
+use Socialtext::JSON qw/decode_json/;
 
 our $k;
 
 # We punt to the permission handling stuff below.
-sub permission { +{ GET => undef } }
+sub permission { +{ GET => undef, PUT => undef } }
 sub entity_name { "User " . $_[0]->username }
 sub accounts { undef };
 
@@ -45,5 +42,51 @@ sub get_resource {
     }
     return undef;
 }
+
+sub PUT_json {
+    my $self = shift;
+    my $rest = shift;
+    my $username = $self->username;
+    return $self->not_authorized unless $rest->user->is_business_admin;
+
+    my $user = eval { Socialtext::User->Resolve($username) };
+    unless ($user) {
+        $rest->header( -status => HTTP_400_Bad_Request );
+        return 'No such user';
+    }
+
+    my $content = $rest->getContent();
+    my $object = eval { decode_json( $content ) };
+    if (!$object or ref($object) ne 'HASH') {
+        $rest->header( -status => HTTP_400_Bad_Request );
+        return 'Content should be a JSON hash.';
+    }
+
+    my $new_acct_id = $object->{primary_account_id};
+    unless ($new_acct_id) {
+        $rest->header( -status => HTTP_400_Bad_Request );
+        return 'No primary_account_id specified!';
+    }
+
+    my $acct = Socialtext::Account->new(account_id => $new_acct_id);
+    unless ($acct) {
+        $rest->header( -status => HTTP_400_Bad_Request );
+        return 'Invalid account ID';
+    }
+
+    eval { $user->primary_account($acct) };
+    if ($@) {
+        warn $@;
+        $rest->header( -status => HTTP_400_Bad_Request );
+        return $@;
+    }
+
+    $rest->header(
+        -status => HTTP_204_No_Content,
+        -Location => "/data/users/" . $user->user_id,
+    );
+    return '';
+}
+
 
 1;
