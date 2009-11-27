a47350a47e12de0db84809410a63bb09465be4b9
Stash <jeremy.stashewsky@socialtext.com>
2009-11-27 15:15:45 -0800

    start mocking Groups

---
+++ nlw/t/lib/Socialtext/Group.pm
@@ -0,0 +1,15 @@
+package Socialtext::Group;
+use unmocked 'Moose';
+
+has 'group_id' => (is => 'rw', isa => 'Int', required => 1);
+
+use unmocked 'constant', is_plugin_enabled => 1;
+
+my %Groups;
+
+sub GetGroup {
+    my $class = shift;
+    return $class->new(@_);
+}
+
+1;
