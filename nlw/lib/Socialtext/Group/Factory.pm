7038cbc85bb2f075452e561b6a9bc2f891d2d203
Kevin Jones <kevin.jones@socialtext.com>
2009-11-30 10:22:53 -0800

    Don't try to create groups with null descriptions

--- nlw/lib/Socialtext/Group/Factory.pm
+++ nlw/lib/Socialtext/Group/Factory.pm
@@ -326,6 +326,8 @@ sub NewGroupRecord {
 
     # new Group records default to being cached _now_.
     $proto_group->{cached_at} ||= $self->Now();
+    delete $proto_group->{description} 
+        unless defined $proto_group->{description};
 
     # Only concern ourselves with valid Db Columns
     my $valid = $self->FilterValidColumns( $proto_group );
