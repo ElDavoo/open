a304edb0195b2289d74993c58a9611641b6d3e15
Audrey Tang <audrey.tang@socialtext.com>
2009-11-30 21:06:18 -0800

    * SC Formatting js-test.

--- nlw/share/skin/js-test/socialcalc/t/format.t.js
+++ nlw/share/skin/js-test/socialcalc/t/format.t.js
@@ -21,33 +21,31 @@ t.runAsync([
     t.doCheckCSS('border', '', 'Border Off'),
 
     function() {
-        t.$('#st-spreadsheet-cell-font-family').val('Arial').change();
+        t.$('#st-spreadsheet-cell-font-family').val('Verdana,Arial,Helvetica,sans-serif').change();
         t.$('#st-spreadsheet-cell-font-size').val(28).change();
         t.$('#st-spreadsheet-cell-number-format').val('#,##0').change();
         t.callNextStep();
     },
 
-    //t.doClick('#st-color-button-link'),
-    //t.doClick('#st-color-ff0000'),
-    //t.doClick('#st-bgcolor-button-link'),
-    //t.doClick('#st-bgcolor-0000ff'),
+    t.doClick('#st-color-button-link'),
+    t.doClick('#st-color-cc0000'),
+    t.doClick('#st-bgcolor-button-link'),
+    t.doClick('#st-bgcolor-ff0000'),
+    t.doClick('#st-swapcolors-button-link'),
 
     function() {
         t.click('#st-preview-button-link');
         t.callNextStepOn('#st-spreadsheet-preview #cell_A1');
     },
 
-    t.doCheckCSS('font-family', 'Arial', 'Set Font'),
+    t.doCheckCSS('font-family', 'Verdana,Arial,Helvetica,sans-serif', 'Set Font'),
     t.doCheckCSS('font-size', '37.3333px', 'Set Size'),
     t.doCheckText('1,234', 'Set Format'),
 
-    //t.doCheckCSS('color', 'Arial', 'Text Color'),
-    //t.doCheckCSS('background-color', 'Arial', 'Background Color'),
-
     function() {
-        t.pass('Text Color'); // XXX TODO
-        t.pass('Background Color'); // XXX TODO
-        t.pass('Swap Color'); // Passed implicitly by color/bgrolocr above
+        t.is(t.$('#st-spreadsheet-preview #cell_A1').css('color'), 'rgb(255, 0, 0)', 'Text Color');
+        t.is(t.$('#st-spreadsheet-preview #cell_A1').css('background-color'), 'rgb(204, 0, 0)', 'Background Color');
+        t.pass('Swap Color'); // Passed implicitly by color/bgcolor above
         t.endAsync();
     }
 ]);
