686d08fd9a5c1818e80ee4e39b0cf25bdfbc02f3
Kevin Jones <kevin.jones@socialtext.com>
2009-12-02 12:51:10 -0800

    Check permissions before displaying a group photo.
    We're always going to return an image, however, if:
    * Group doesn't exist, return a 404 and the Default image
    * User is not in Group, return a 401 and the Default image
    * User is in Group, return 200 and the Group's image

--- nlw/lib/Socialtext/Rest/Group/Photo.pm
+++ nlw/lib/Socialtext/Rest/Group/Photo.pm
@@ -4,26 +4,46 @@ use strict;
 use warnings;
 use Socialtext::File;
 use Socialtext::Group;
+use Socialtext::Group::Photo;
+use Socialtext::HTTP ':codes';
 
 use base 'Socialtext::Rest';
+
+sub group {
+   return Socialtext::Group->GetGroup( group_id => shift->group_id );
+}
 
 sub _get_photo {
     my $self = shift;
     my $rest = shift;
     my $size = shift || 'large';
 
-    my $group = Socialtext::Group->GetGroup(group_id => $self->group_id);
+    my $user  = $rest->user;
+    my $group = $self->group;
 
-    # send the default photo back, but such that it'll get re-requested again
-    # once per page.
-    my %headers = $rest->header;
+    my ($photo, $status);
+    if ( $group ) {
+        if ( $group->has_user($user) ) {
+            $status = HTTP_200_OK;
+            $photo  = $group->photo->$size;
+        }
+        else {
+            $status = HTTP_401_Unauthorized;
+            $photo  = Socialtext::Group::Photo->DefaultPhoto($size);
+        }
+    }
+    else{
+        $status = HTTP_404_Not_Found;
+        $photo  = Socialtext::Group::Photo->DefaultPhoto($size);
+    }
+
     $rest->header(
-        -status        => $headers{-status},
+        -status        => $status,
         -pragma        => 'no-cache',
         -cache_control => 'no-cache, no-store',
         -type          => 'image/png',
     );
-    return $group->photo->$size;
+    return $photo;
 }
 
 sub GET_photo {
