df78c5543911892ecbc741c727f3daad75752d5e
Kevin Jones <kevin.jones@socialtext.com>
2009-12-07 11:29:15 -0800

    Fix Socialtext::Users::Find queries

--- nlw/lib/Socialtext/User/Find.pm
+++ nlw/lib/Socialtext/User/Find.pm
@@ -43,23 +43,21 @@ sub get_results {
     my $self = shift;
 
     my $sql = q{
-        SELECT user_id, first_name, last_name, email_address, driver_username
-        FROM users
-        WHERE (
+        SELECT DISTINCT other.user_id, first_name, last_name,
+                        email_address, driver_username
+          FROM user_sets_for_user viewer
+          JOIN user_sets_for_user other USING (user_set_id)
+          JOIN users ON (other.user_id = users.user_id)
+         WHERE viewer.user_id = $2
+           AND (
             lower(first_name) LIKE $1 OR
             lower(last_name) LIKE $1 OR
             lower(email_address) LIKE $1 OR
             lower(driver_username) LIKE $1 OR
             lower(display_name) LIKE $1
-        )
-        AND EXISTS (
-            SELECT 1
-            FROM account_user viewer
-            JOIN account_user found USING (account_id)
-            WHERE viewer.user_id = $2 AND found.user_id = users.user_id
-        )
-        ORDER BY last_name ASC, first_name ASC
-        LIMIT $3 OFFSET $4
+         )
+         ORDER BY last_name ASC, first_name ASC
+         LIMIT $3 OFFSET $4
     };
 
     #local $Socialtext::SQL::PROFILE_SQL = 1;
--- nlw/lib/Socialtext/User/Find/Workspace.pm
+++ nlw/lib/Socialtext/User/Find/Workspace.pm
@@ -15,52 +15,37 @@ sub get_results {
     my $self = shift;
 
     my $sql = q{
-        SELECT user_id, first_name, last_name, email_address, driver_username,
-            role_names
-        FROM users
-        JOIN (
-            SELECT user_id, array_accum(DISTINCT "Role".name) AS role_names
-            FROM all_user_workspace_role
-            JOIN "Role" USING (role_id)
-            WHERE workspace_id = $3
-            GROUP BY user_id
-        ) all_roles USING (user_id)
-        WHERE
-        (
+        SELECT user_id, first_name, last_name,
+               email_address, driver_username, "Role".name AS role_name
+        FROM user_set_path
+        JOIN users ON (from_set_id = user_id)
+        JOIN "Role" USING(role_id)
+        WHERE into_set_id = $3
+          AND EXISTS (
+            SELECT 1
+              FROM user_sets_for_user
+             WHERE user_set_id = $3
+               AND user_id = $2
+          )
+          AND (
             lower(first_name) LIKE $1 OR
             lower(last_name) LIKE $1 OR
             lower(email_address) LIKE $1 OR
             lower(driver_username) LIKE $1
-        )
-        AND EXISTS (
-            SELECT 1
-            FROM account_user viewer
-            JOIN account_user found USING (account_id)
-            WHERE viewer.user_id = $2 AND found.user_id = users.user_id
-        )
+          )
         ORDER BY last_name ASC, first_name ASC
         LIMIT $4 OFFSET $5
     };
 
     #local $Socialtext::SQL::PROFILE_SQL = 1;
     my $sth = sql_execute($sql, 
-        $self->filter, $self->viewer->user_id, $self->workspace->workspace_id,
+        $self->filter, $self->viewer->user_id, $self->workspace->user_set_id,
         $self->limit, $self->offset,
     );
 
     my $rows = $sth->fetchall_arrayref({}) || [];
     for my $row (@$rows) {
-        my @roles = map { Socialtext::Role->new(name => $_) }
-            @{$row->{role_names} || []};
-
-        my @sorted_role_names = map { $_->name }
-            Socialtext::Role->SortByEffectiveness(roles => \@roles);
-
-        delete $row->{role_names};
-        $row->{role_name} = $sorted_role_names[-1];
-        $row->{roles} = \@sorted_role_names;
-        $row->{is_workspace_admin} =
-            any { $_ eq 'admin' } @sorted_role_names;
+        $row->{is_workspace_admin} = $row->{role_name} eq 'admin';
     }
 
     return $rows;
