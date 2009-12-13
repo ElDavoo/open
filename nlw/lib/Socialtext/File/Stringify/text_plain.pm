6e2c5327172b8a4c878dce7d3c1e67c6f5a3d4c3
swdevlin <shawn.devlin@socialtext.com>
2009-12-12 22:59:26 -0800

    Some binary files with .txt extension segfault Perl when we read them. au suggested a -T check on the file first before we attempt to read the file contents.

--- nlw/lib/Socialtext/File/Stringify/text_plain.pm
+++ nlw/lib/Socialtext/File/Stringify/text_plain.pm
@@ -9,6 +9,7 @@ use Socialtext::l10n qw(system_locale);
 sub to_string {
     my ( $class, $filename ) = @_;
     my $text = '';
+    return $text unless (-T $filename);
     eval {
         my $encoding = Socialtext::File::get_guess_encoding(system_locale(), $filename);
         $text = scalar Socialtext::File::get_contents_based_on_encoding($filename, $encoding);
