b37c53941e61f8345cd0c44dee4eb3caf648bfc4
Luke Closs <gravatar@5thplane.com>
2009-12-15 11:12:41 -0800

    insert before indexs for speed; fix version; fix column type

--- nlw/etc/socialtext/db/socialtext-99-to-100.sql
+++ nlw/etc/socialtext/db/socialtext-99-to-100.sql
@@ -5,7 +5,7 @@ BEGIN;
 
 CREATE TABLE signal_user_set (
     signal_id bigint NOT NULL,
-    user_set_id bigint NOT NULL
+    user_set_id int NOT NULL
 );
 
 ALTER TABLE ONLY signal_user_set
@@ -17,20 +17,20 @@ ALTER TABLE ONLY signal_user_set
         FOREIGN KEY (signal_id)
         REFERENCES signal (signal_id) ON DELETE CASCADE;
 
+INSERT INTO signal_user_set (signal_id, user_set_id)
+    SELECT signal_id, account_id + x'30000000'::int as user_set_id
+      FROM signal_account;
+
 CREATE INDEX ix_signal_user_set
     ON signal_user_set (signal_id);
 
 CREATE UNIQUE INDEX ix_signal_user_set_user_set
     ON signal_user_set (user_set_id, signal_id);
 
-INSERT INTO signal_user_set (signal_id, user_set_id)
-    SELECT signal_id, account_id + x'30000000'::int as user_set_id
-      FROM signal_account;
-
 DROP TABLE signal_account;
 
 UPDATE "System"
-   SET value = '99'
+   SET value = '100'
  WHERE field = 'socialtext-schema-version';
 
 COMMIT;
