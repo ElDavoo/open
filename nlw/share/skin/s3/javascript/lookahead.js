0b19bf5b04727720e6194888a60e6704f4640b54
Audrey Tang <audrey.tang@socialtext.com>
2009-12-16 05:30:13 -0800

    Revert "* Fix {bz: 2543} by not relying on the non-CJK-friendly \b provided by client JS."
    This reverts commit 9583f2f8fa49de061c12c1f91273006116c4b964.

--- nlw/share/skin/s3/javascript/lookahead.js
+++ nlw/share/skin/s3/javascript/lookahead.js
@@ -232,13 +232,15 @@
             if (filtered.length >= self.opts.count) return;
 
             var title = self.linkTitle(this);
-            if (self.opts.grep && !self.opts.grep(this)) return;
+            if (title.match(re)) {
+                if (self.opts.grep && !self.opts.grep(this)) return;
 
-            filtered.push({
-                bolded_title: title.replace(re, '<b>$1</b>'),
-                title: title,
-                value: self.linkValue(this)
-            });
+                filtered.push({
+                    bolded_title: title.replace(re, '<b>$1</b>'),
+                    title: title,
+                    value: self.linkValue(this)
+                });
+            }
         });
 
         return filtered;
