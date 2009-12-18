2c529777896b99ebf39e3ea8ced833b7d225c277
Kevin Jones <kevin.jones@socialtext.com>
2009-12-18 10:12:44 -0800

    Allow disabling the timer report, which makes it impossible to see errors without scrolling

--- nlw/lib/Test/Socialtext.pm
+++ nlw/lib/Test/Socialtext.pm
@@ -296,7 +296,7 @@ sub timer_clear {
 }
 
 sub timer_report {
-    return unless ($ENV{TEST_VERBOSE});
+    return unless ($ENV{TEST_VERBOSE} and not $ENV{NO_TIMER_REPORT});
 
     diag "Socialtext::Timer report\n";
     my $report = Socialtext::Timer->Report();
