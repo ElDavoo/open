0720007f144bb8335ec90ef9229613b6b25e46c3
Kevin Jones <kevin.jones@socialtext.com>
2010-02-02 13:20:21 -0800

    Make startIndex smarter and use $self->pageable for determining whether to page data

--- nlw/lib/Socialtext/Rest/Pageable.pm
+++ nlw/lib/Socialtext/Rest/Pageable.pm
@@ -6,10 +6,17 @@ use constant max_per_page => 100;
 
 requires qw(_get_total_results _get_entities);
 
-has 'start_index' => ( is => 'ro', isa => 'Maybe[Int]', lazy_build => 1 );
+has 'pageable' => ( is => 'ro', isa => 'Bool', lazy_build => 1 );
+sub _build_pageable {
+    my $self = shift;
+    defined $self->rest->query->param('startIndex');
+}
+
+has 'start_index' => ( is => 'ro', isa => 'Int', lazy_build => 1 );
 sub _build_start_index {
     my $self = shift;
-    $self->rest->query->param('startIndex');
+    my $index = $self->rest->query->param('startIndex');
+    return defined $index ? $index : $self->rest->query->param('offset') || 0;
 }
 
 has 'items_per_page' => ( is => 'ro', isa => 'Int', lazy_build => 1 );
@@ -25,7 +32,7 @@ sub _build_reverse {
     $self->rest->query->param('reverse');
 }
 
-has 'order' => ( is => 'ro', isa => 'Str', lazy_build => 1 );
+has 'order' => ( is => 'ro', isa => 'Maybe[Str]', lazy_build => 1 );
 sub _build_order {
     my $self = shift;
     $self->rest->query->param('order');
@@ -42,7 +49,7 @@ sub get_resource {
     @$results = map { $self->_entity_hash($_) } @$results;
     Socialtext::Timer->Pause('_entity_hash_map');
 
-    if (defined $self->start_index and $content_type eq 'application/json') {
+    if ($self->pageable and $content_type eq 'application/json') {
         return {
             startIndex => $self->start_index+0,
             itemsPerPage => $self->items_per_page+0,
