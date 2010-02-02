7a4229998d562f62d37a2be6ef99888e593f80a0
Kevin Jones <kevin.jones@socialtext.com>
2010-02-01 17:34:45 -0800

    Allow updating a group's role in a workspace

--- nlw/lib/Socialtext/Rest/WorkspaceGroup.pm
+++ nlw/lib/Socialtext/Rest/WorkspaceGroup.pm
@@ -5,52 +5,111 @@ use Moose;
 use Socialtext::HTTP ':codes';
 use Socialtext::Group;
 use Socialtext::Workspace;
+use Socialtext::Permission qw(ST_ADMIN_PERM);
+use Socialtext::Exceptions qw(conflict rethrow_exception);
+use Socialtext::SQL qw(get_dbh :txn);
+use Socialtext::JSON qw/decode_json/;
 use namespace::clean -except => 'meta';
 
 extends 'Socialtext::Rest::Entity';
 
 sub allowed_methods { 'DELETE' };
 
+has 'target_group' => (
+    is => 'ro', isa => 'Socialtext::Group', lazy_build => 1,
+);
+
+sub _build_target_group {
+    my $self = shift;
+    my $group_id = $self->group_id;
+    my $group = Socialtext::Group->GetGroup(group_id => $group_id);
+}
+
+sub if_authorized {
+    my $self = shift;
+    my $call = shift;
+
+    my $acting_user = $self->rest->user;
+    my $checker = $self->hub->checker;
+
+    return $self->no_workspace() unless $self->workspace;
+    return $self->not_authorized() unless $self->can_admin;
+    unless ($self->workspace->has_group($self->target_group)) {
+        $self->rest->header( -status => HTTP_404_Not_Found );
+        return $self->target_group->driver_group_name
+             . " is not a member of "
+             . $self->workspace->name;
+    }
+    return $self->$call(@_);
+}
+
 # Remove a Group from a Workspace
 sub DELETE {
     my ($self, $rest) = @_;
-
-    # Make sure we have a Workspace to work against.
-    my $workspace = $self->workspace();
-    return $self->no_workspace() unless $workspace;
-
-    # Make sure we've got sufficient privs to do the DELETE
-    unless ($self->_have_admin_privs()) {
-        return $self->not_authorized();
-    }
-
-    # Get the Group
-    my $group_id = $self->group_id();
-    my $group = Socialtext::Group->GetGroup(group_id => $group_id);
-    unless ($group) {
-        $rest->header( -status => HTTP_404_Not_Found );
-        return "Unable to find Group $group_id";
-    }
-
-    # Check if Group has _some_ Role in the WS
-    unless ($workspace->has_group($group)) {
-        $rest->header( -status => HTTP_404_Not_Found );
-        return $group->driver_group_name
-             . " is not a member of "
-             . $workspace->name;
-    }
-
-    # Remove the Group from the WS
-    $workspace->remove_group( group => $group );
-    $rest->header( -status => HTTP_204_No_Content );
-    return '';
+    $self->if_authorized(sub {
+        # Remove the Group from the WS
+        $self->workspace->remove_group( group => $self->target_group );
+        $rest->header( -status => HTTP_204_No_Content );
+        return '';
+    });
 }
 
-sub _have_admin_privs {
+# Remove a Group from a Workspace
+sub PUT {
+    my ($self, $rest) = @_;
+    $self->if_authorized(sub {
+        my $content = $rest->getContent();
+
+        my $dbh = get_dbh();
+        my $in_txn = sql_in_transaction();
+        $dbh->begin_work unless $in_txn;
+
+        eval {
+            my $object = decode_json( $content );
+            die 'role parameter is required' unless $object->{role_name};
+
+            my $role = Socialtext::Role->new(name => $object->{role_name});
+            die "role '$object->{role_name}' doesn't exist" unless $role;
+
+            $self->workspace->assign_role_to_group(
+                group => $self->target_group, role => $role
+            );
+
+            my $admins = $self->workspace->role_count(
+                role => Socialtext::Role->Admin(),
+                direct => 1,
+            );
+            conflict errors => ["cannot delete last admin"] unless $admins;
+
+            $dbh->commit unless $in_txn;
+        };
+        my $e = Exception::Class->caught('Socialtext::Exception::Conflict');
+        if ($e) {
+            $dbh->rollback unless $in_txn;
+            return $self->conflict($e->errors);
+        }
+        elsif ( $@ )  {
+            warn $@;
+            $dbh->rollback unless $in_txn;
+            $rest->header( -status => HTTP_400_Bad_Request );
+            return $@;
+        }
+        
+        $rest->header( -status => HTTP_204_No_Content );
+        return '';
+    });
+}
+
+sub can_admin {
     my $self = shift;
+
     return $self->rest->user->is_business_admin()
         || $self->rest->user->is_technical_admin()
-        || $self->hub->checker->check_permission('admin_workspace');
+        || $self->hub->checker->check_permission('admin_workspace')
+        || $self->target_group->user_can(
+            user => $self->rest->user,
+            permission => ST_ADMIN_PERM,
+        );
 }
 
 no Moose;
