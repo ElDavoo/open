44359f86d0f0f28e4ad7b044d37c5b10f1517830
Graham TerMarsch <graham.termarsch@socialtext.com>
2010-02-08 23:56:17 -0800

    OpenToken authentication.  Allows integration with SAML providers like Ping Identity.

---
+++ nlw/lib/Socialtext/Challenger/OpenToken.pm
@@ -0,0 +1,225 @@
+package Socialtext::Challenger::OpenToken;
+# @COPYRIGHT@
+
+use strict;
+use warnings;
+use Crypt::OpenToken;
+use URI;
+use Socialtext::Apache::User;
+use Socialtext::Log qw(st_log);
+use Socialtext::OpenToken::Config;
+        # username
+        # email address
+        # first name
+        # last name
+use Socialtext::User;
+use Socialtext::WebApp;
+
+our $DEFAULT_REDIRECT_URI = '/';
+
+sub challenge {
+    my $class   = shift;
+    my %p       = @_;
+    my $hub     = $p{hub};
+    my $request = $p{request};
+
+    # make sure we've got a request
+    my $app = Socialtext::WebApp->NewForNLW;
+    unless ($request) {
+        $request = $app->apache_req;
+    }
+
+    # get a handle to the OpenToken configuration
+    my $config = Socialtext::OpenToken::Config->load();
+    unless ($config) {
+        return $app->_handle_error(
+            error => 'OpenToken configuration missing/invalid.',
+            path  => '/nlw/login.html',
+        );
+    }
+
+    # if we have a Hub *AND* a User, we're Authentiated but not Authorized;
+    # show the User a page letting them know that they don't have permission.
+    if ($hub and not $hub->current_user->is_guest) {
+        # XXX: we -DON'T- want to use "/nlw/login.html" here, but don't have
+        # any other choice at the moment.  Ideally, we want some sort of error
+        # page that shows a "you're not authorized" error but that -DOESN'T-
+        # include a login box on the page (as we're not responsible for the
+        # logins).
+        st_log->debug( 'ST::Challenger::OpenToken: unauthorized access, showing error page to user' );
+        return $app->_handle_error(
+            error => {
+                type => 'unauthorized_workspace',
+            },
+            path    => '/nlw/login.html',
+        );
+    }
+
+    # figure out where we need to redirect the User to in order to get an
+    # OpenToken, in the event that we're either not Authenticated or we have
+    # some problem with the OpenToken.
+    my $challenge_uri = $class->build_challenge_uri($config, $request);
+
+    # get the OpenToken as provided in the "opentoken" form parameter
+    my $token_param = $config->token_parameter;
+    my $token_str   = $request->param($token_param);
+    unless ($token_str) {
+        st_log->info("ST::Challenger::OpenToken: no token provided, redirecting");
+        return $app->redirect($challenge_uri);
+    }
+
+    # parse/dissect the OpenToken
+    my $factory = Crypt::OpenToken->new(password => $config->password);
+    my $token   = eval { $factory->parse($token_str) };
+    if ($@) {
+        st_log->warning("ST::Challenger::OpenToken: error occurred while parsing token; $@");
+        return $app->redirect($challenge_uri);
+    }
+    unless ($token) {
+        st_log->warning("ST::Challenger::OpenToken: unable to parse token; $@");
+        return $app->redirect($challenge_uri);
+    }
+
+    # make sure that the token is valid
+    my $skew = $config->clock_skew();
+    unless ($token->is_valid(clock_skew => $skew)) {
+        st_log->info("ST::Challenger::OpenToken: invalid token, redirecting");
+        return $app->redirect($challenge_uri);
+    }
+
+    # extract the username from the token, and go lookup the User
+    my $username = $token->subject();
+    my $user     = eval { Socialtext::User->new(username => $username) };
+    unless ($user) {
+        my $auto_provision = $config->auto_provision_new_users;
+        if ($auto_provision) {
+die "XXX : not implemented yet -- autoprovision new users\n";
+        }
+        else {
+            st_log->warning("ST::Challenger::OpenToken: have valid token, but for unknown user '$username'");
+            return $app->_handle_error(
+                error => {
+                    type => 'not_logged_in',
+                },
+                path    => '/nlw/login.html',
+            );
+        }
+    }
+
+    # figure out where the User wanted to be in the first place, and redirect
+    # them off over there.
+    my $redirect = $class->get_redirect_uri($token->data->{resource_url});
+    st_log->info("LOGIN: " . $user->email_address . " destination $redirect");
+    Socialtext::Apache::User::set_login_cookie($request, $user->user_id, '');
+    $user->record_login;
+    return $app->redirect($redirect);
+}
+
+# Returns the URL which kickstarts a SP-initiated SAML assertion.
+sub build_challenge_uri {
+    my ($class, $config, $request) = @_;
+
+    # start with the configured Challenge URI
+    my $challenge_uri = URI->new($config->challenge_uri);
+
+    # add in the "resource_url" (the URL the User was _trying_ to get to)
+# XXX: does this get auto-escaped for us? or do we have to do it ourselves?
+    $challenge_uri->query_form(resource_url => $request->parsed_uri->unparse);
+
+    # done
+    return $challenge_uri->as_string;
+}
+
+# Returns the URI to redirect the User to *after* a successful Authen
+# handshake.
+#
+# We do *not* allow for redirects to send the User to a machine other than
+# ourselves.
+sub get_redirect_uri {
+    my ($self, $redirect) = @_;
+
+    # get the URI that we're supposed to redirect the User to, and make sure
+    # that its a *relative* URI (no absolute URIs allowed)
+    $redirect ||= $DEFAULT_REDIRECT_URI;
+    my $uri = URI->new($redirect);
+    if ($uri->scheme) {
+        # given an absolute URI; if it points to somewhere _other_than_
+        # this machine, fail
+        my $host      = $uri->host();
+        my $this_host = Socialtext::AppConfig->web_hostname();
+        if ($host ne $this_host) {
+            st_log->error(
+                "ST::Challenger::OpenToken; redirect attempted to external source; $redirect"
+            );
+            return $DEFAULT_REDIRECT_URI;
+        }
+    }
+
+    # return the relative form of the URI back to the caller.
+    return $uri->path_query();
+}
+
+1;
+
+=head1 NAME
+
+Socialtext::Challenger::OpenToken - Custom challenger for OpenToken integration
+
+=head1 SYNOPSIS
+
+  Do not instantiate this class directly.
+  Use Socialtext::Challenger instead.
+
+=head1 DESCRIPTION
+
+When configured for use, this Challenger redirects Users off to an OpenToken
+Service Provider to perform authentication.
+
+The Service Provider will then verify the User's identity against an Identity
+Provider and then redirect the User back to us with an OpenToken.  This
+OpenToken will then contain the information that we need to know about this
+User in order to authenticate them and log them in to the system.
+
+This module can be used to integrate a Socialtext Appliance into a SAML
+infrastructure using a SAML Service Provider application such as PingFederate.
+
+=head1 METHODS
+
+=over
+
+=item B<Socialtext::Challenger::OpenToken-E<gt>challenge(%p)>
+
+Custom challenger.
+
+Not to be called directly.  Use C<Socialtext::Challenger> instead.
+
+=item B<Socialtext::Challenger::OpenToken-E<gt>build_challenge_uri($config, $request)>
+
+Builds the URI that the User should be redirected to in order to get
+Authenticated.
+
+=item B<Socialtext::Challenger::OpenToken-E<gt>get_redirect_uri($redirect)>
+
+Returns the URI to redirect the User to B<after> a successful Authentication
+has been performed.
+
+Redirects to I<external> URLs is B<forbidden>.  Attempts to redirect the User
+off of the Socialtext Appliance to an external URL will result in an error
+being logged and the User being redirected to the default URI ("/").
+
+=back
+
+=head1 AUTHOR
+
+Socialtext, Inc. C<< <code@socialtext.com> >>
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2010 Socialtext, Inc., All Rights Reserved.
+
+=head1 SEE ALSO
+
+L<Socialtext::Challenger>,
+L<Crypt::OpenToken>.
+
+=cut
