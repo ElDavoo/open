a2e10e2e27e7cb34dd0368b685d5c610e2df688e
Ken Pier <ken.pier@socialtext.com>
2010-02-10 16:18:37 -0800

    bugfix version bump

--- nlw/lib/Socialtext.pm
+++ nlw/lib/Socialtext.pm
@@ -16,7 +16,7 @@ use Socialtext::Authz;
 use Socialtext::Permission 'ST_READ_PERM';
 use Socialtext::Validate qw( validate SCALAR_TYPE USER_TYPE WORKSPACE_TYPE );
 
-our $VERSION = '3.7.4.13';
+our $VERSION = '3.7.4.14';
 
 const product_version => $VERSION;
 field using_debug => 0;
