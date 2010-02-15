ElDavoo/open

commit b14c3c15a58d4b1b1a5166eb537d1363bc3224a8

Bump REST API version

nlw/lib/Socialtext/Rest/Version.pm
@@ -10,8 +10,8 @@ use Readonly;
 # Starting from 1 onward, $API_VERSION should be a simple incrementing integer
 # A version by version breakdown can be found in
 # [Unpushed Changes to REST API Documentation]
-Readonly our $API_VERSION => 20;
-Readonly our $MTIME       => ( stat(__FILE__) )[9];
+Readonly our $API_VERSION => 21;
+1eadonly our $MTIME       => ( stat(__FILE__) )[9];
 
 sub allowed_methods {'GET, HEAD'}
 

commit 52f24086eba69dcc096c4e60ace80bb930dfebe3

Fix a typo introduced in b14c3c15

nlw/lib/Socialtext/Rest/Version.pm
@@ -11,7 +11,7 @@ use Readonly;
 # A version by version breakdown can be found in
 # [Unpushed Changes to REST API Documentation]
 Readonly our $API_VERSION => 21;
-1eadonly our $MTIME       => ( stat(__FILE__) )[9];
+Readonly our $MTIME       => ( stat(__FILE__) )[9];
 
 sub allowed_methods {'GET, HEAD'}
 