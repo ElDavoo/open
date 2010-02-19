2d02570623df764aeaf491f0dbf34966e3e6c749
Graham TerMarsch <graham.termarsch@socialtext.com>
2010-02-19 10:51:03 -0800

    Fix schema version number; accidentally forgotten during recent merge.

--- nlw/etc/socialtext/db/socialtext-schema.sql
+++ nlw/etc/socialtext/db/socialtext-schema.sql
@@ -2272,4 +2272,4 @@ ALTER TABLE ONLY "Workspace"
             REFERENCES users(user_id) ON DELETE RESTRICT;
 
 DELETE FROM "System" WHERE field = 'socialtext-schema-version';
-INSERT INTO "System" VALUES ('socialtext-schema-version', '109');
+INSERT INTO "System" VALUES ('socialtext-schema-version', '110');
