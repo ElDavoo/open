905ef8e187191c461fc4230c7863db270099b6e3
Gabe Wachob <gwachob@wachob.com>
2010-02-18 17:33:59 -0800

    Make adding a user to an existing group properly send email invite

--- nlw/share/skin/common/javascript/Socialtext/lib/Socialtext/Group.js
+++ nlw/share/skin/common/javascript/Socialtext/lib/Socialtext/Group.js
@@ -90,8 +90,11 @@ $.extend(Socialtext.Group.prototype, {
      *   }
      */
     addMembers: function(users, callback) {
-        var users = users && users.users ? users.users : users || [];
-        if (!users.length) return callback({});
+        // Not an array, then make it into a structure
+        if ($.isArray(users) || !users) { 
+            users = {users: (users || [])};
+        };
+        if (!users.users.length) return callback({});
         this.postItems(this.url('/users'), users, callback);
     },
 
