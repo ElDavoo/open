a27a2268d1afde67bf4497a849d99b14c44d41c4
Luke Closs <gravatar@5thplane.com>
2010-02-23 12:39:49 -0800

    Make the PageViewer REST call pageable

--- nlw/lib/Socialtext/Rest/Report/PageViewers.pm
+++ nlw/lib/Socialtext/Rest/Report/PageViewers.pm
@@ -5,6 +5,7 @@ use Socialtext::JSON qw/encode_json/;
 use namespace::clean -except => 'meta';
 
 extends 'Socialtext::Rest::ReportAdapter';
+with 'Socialtext::Rest::Pageable';
 
 =head1 NAME
 
@@ -20,11 +21,37 @@ Shows the people that viewed the given page recently.
 
 =cut
 
+# Note: We're not using Socialtext::Rest::Pageable's get_resource() because
+# we do not need to provide backwards compatible interfaces, and we want
+# more control over the REST responses.
+
+sub start_time { 'now' }
+sub duration { '-3months' }
+
 override 'GET_json' => sub {
+    my $self = shift;
+
+    return $self->error(400, 'Bad request', $@) if $@;
+
+    my $data = eval { $self->_get_entities() };
+    return $data unless ref($data);
+    return $self->error(400, 'Bad request', $@) if $@;
+
+    $self->rest->header(-type => 'application/json');
+    return encode_json({
+        startIndex => $self->start_index+0,
+        itemsPerPage => $self->items_per_page+0,
+        totalResults => $self->_get_total_results,
+        start_time => $self->start_time(),
+        duration   => $self->duration(),
+        entry => [ grep {defined} map { $self->_entity_hash($_) } @$data ],
+    });
+};
+
+sub _get_entities {
     my $self = shift;
     my $user = $self->rest->user;
     my $page = $self->page;
-
     my $report = eval { $self->adapter->_build_report(
         'ViewersByPage', {
             start_time  => 'now',
@@ -34,42 +61,39 @@ override 'GET_json' => sub {
             page_id     => $page->id,
         }, $user,
     ) };
-    return $self->error(400, 'Bad request', $@) if $@;
     return $self->not_authorized unless $report->is_viewable_by($user);
+    my $all_data = $report->_data;
+    $self->{_total_results} = @$all_data;
+    return [
+        splice @$all_data, $self->start_index, $self->items_per_page
+    ];
+}
 
-    my @users;
-    eval { 
-        my $data = $report->_data;
-        use Data::Dumper;
-        warn Dumper $data;
-        # Clean up the data
-        for my $row (@$data) {
-            my ($username, $count) = @$row;
+sub _get_total_results {
+    my $self = shift;
+    return $self->{_total_results};
+}
 
-            my $user = Socialtext::User->Resolve($username);
-            warn "no user $username" unless $user;
-            next unless $user;
+sub _entity_hash {
+    my $self = shift;
+    my $obj  = shift;
 
-            # ASS-U-ME that if they can view the same page as you, you
-            # can see their profile.
-            my $user_id = $user->user_id;
-            push @users, {
-                title          => $user->guess_real_name,
-                uri            => "/st/profile/$user_id",
-                is_person      => 1,
-                user_id        => $user_id,
-                count          => $count,
-                context_title  => $user->primary_account->name,
-            };
-        }
+    my ($username, $count) = @$obj;
+    my $user = Socialtext::User->Resolve($username);
+    return undef unless $user;
+
+    # ASS-U-ME that if they can view the same page as you, you
+    # can see their profile.
+    my $user_id = $user->user_id;
+    return {
+        title          => $user->guess_real_name,
+        uri            => "/st/profile/$user_id",
+        is_person      => 1,
+        user_id        => $user_id,
+        count          => $count,
+        context_title  => $user->primary_account->name,
     };
-    return $self->error(400, 'Bad request', $@) if $@;
-
-    $self->rest->header(-type => 'application/json');
-    return encode_json({
-        rows => \@users,
-    });
-};
+}
 
 __PACKAGE__->meta->make_immutable(inline_constructor => 0);
 1;
