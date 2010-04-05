bf418a534f6c0386bd30a197f2c6f8cf1392e9e3
Luke Closs <gravatar@5thplane.com>
2010-04-05 15:31:18 -0700

    bump schema version to match recent migration

--- nlw/etc/socialtext/db/socialtext-schema.sql
+++ nlw/etc/socialtext/db/socialtext-schema.sql
@@ -2302,4 +2302,4 @@ ALTER TABLE ONLY "Workspace"
             REFERENCES users(user_id) ON DELETE RESTRICT;
 
 DELETE FROM "System" WHERE field = 'socialtext-schema-version';
-INSERT INTO "System" VALUES ('socialtext-schema-version', '113');
+INSERT INTO "System" VALUES ('socialtext-schema-version', '114');
