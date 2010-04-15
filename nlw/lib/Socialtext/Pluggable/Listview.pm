9132ce972161714bc005ef1e7b9e67ec41c36e06
Luke Closs <gravatar@5thplane.com>
2010-04-15 11:04:14 -0700

    More refactoring ...

--- nlw/lib/Socialtext/Pluggable/Listview.pm
+++ nlw/lib/Socialtext/Pluggable/Listview.pm
@@ -12,18 +12,18 @@ sub _prepare_listview {
     my $is_search = shift;
 
     my %cgi_vars = $self->cgi_vars;
-
     my $user = $self->user;
+    my $plugin_name = $self->name;
 
     eval { $self->_check_user($user) };
     $self->redirect('/') if $@;
 
     my ($accounts,$acct_group_set,$group_count) =
-        $user->accounts_and_groups(plugin => 'people');
+        $user->accounts_and_groups(plugin => $plugin_name);
     my %acct_set = map { $_->account_id => 1 } @$accounts;
 
     Socialtext::Exception::Auth->throw(
-        loc("not authorized: No Accounts have People enabled")."\n")
+        loc("not authorized: No Accounts have [_1] enabled", $plugin_name)."\n")
         unless @$accounts;
 
     my ($account_id, $all_accounts, $group_id);
@@ -49,7 +49,7 @@ sub _prepare_listview {
         else {
             $account_id = $user->primary_account_id;
             if (!$acct_set{$account_id}) {
-                # pick some people-enabled account
+                # pick some account
                 $account_id = $accounts->[0]->account_id;
             }
         }
@@ -58,21 +58,8 @@ sub _prepare_listview {
     Socialtext::Exception::Auth->throw(loc("Not authorized to use this Account")."\n")
         unless ($account_id eq 'all' || $acct_set{$account_id});
 
-    my $fields = 'email,position,location,work_phone';
-    if ($all_accounts) {
-        $fields .= ',accounts';
-    }
-
-    my $sortby;
-
-    if ($is_search) {
-        $sortby = $self->_store_and_get_search_sort_order;
-    } else {
-        $sortby = $cgi_vars{sortby} || 'best_full_name';
-    }
-    
+    my $sortby = $self->_calc_sortby($is_search, \%cgi_vars);;
     my $tag = delete $cgi_vars{tag};
-
     my $pageset = Socialtext::Pageset->new(
         cgi => {$self->cgi_vars},
         page_size => 20,
@@ -80,7 +67,6 @@ sub _prepare_listview {
     );
 
     my @list_args = (
-        fields => $fields,
         sort => $sortby,
         viewer => $user,
         limit => $pageset->limit,
@@ -90,30 +76,22 @@ sub _prepare_listview {
     push @list_args, account_id => $account_id unless $all_accounts;
     push @list_args, group_id => $group_id if $group_id;
 
-    my $title = $tag ? 
-        loc("All People Tagged '[_1]'", $tag) :
-        loc("All People");
-
+    my $title = $self->_calc_title($tag);
     my @common = (
         unescaped_search_term => $cgi_vars{search_term},
         search_term => Socialtext::String::uri_escape($cgi_vars{search_term}),
         html_escaped_search_term =>
             Socialtext::String::html_escape($cgi_vars{search_term}),
+        account_id => $account_id,
+        group_id => $group_id, #maybe need name?
     );
 
     my @template_args = (
-        people_search => 1,
-        container => { type => 'peoplelist' },
         viewer => $user,
         tag => $tag,
-        predicate => 'action=people', # can be modified by caller
         title => $title,
         display_title => $title,
         sort_by => $sortby,
-        people_selector => $group_id ? ";group_id=$group_id" :
-                            ";account_id=$account_id",
-        account_id => $account_id,
-        group_id => $group_id, #maybe need name?
         accounts => $accounts,
         account_groups => $acct_group_set,
         @common,
@@ -123,7 +101,6 @@ sub _prepare_listview {
         all_accounts => $all_accounts,
         pageset      => $pageset,
         acct_set     => \%acct_set,
-        group_id     => $group_id,
         tag          => $tag,
         scope => $cgi_vars{scope} || '_',
         @common,
