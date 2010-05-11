b302ddc7eaddf76455d9deb2b6ae6bc71cc887e8
Brandon Noard <brandon.noard@socialtext.com>
2010-05-11 16:18:56 -0700

    ReST Role for handlers dealing with a single group

--- nlw/lib/Socialtext/Rest/Group/Users.pm
+++ nlw/lib/Socialtext/Rest/Group/Users.pm
@@ -13,38 +13,7 @@ use Socialtext::l10n qw(loc);
 use namespace::clean -except => 'meta';
 
 extends 'Socialtext::Rest::Users';
-
-has 'group' => (is => 'ro', isa => 'Maybe[Socialtext::Group]', lazy_build => 1);
-has 'user_is_related' => (is => 'ro', isa => 'Bool', lazy_build => 1);
-# see builder for definition of "visitor"
-has 'user_is_visitor' => (is => 'ro', isa => 'Bool', lazy_build => 1);
-
-sub _build_group {
-    my $self = shift;
-
-    my $group_id = $self->group_id;
-    my $group = Socialtext::Group->GetGroup(group_id => $group_id);
-    return $group;
-}
-
-sub _build_user_is_related {
-    my $self = shift;
-    return $self->hub->authz->user_sets_share_an_account(
-        $self->rest->user, $self->group);
-}
-
-sub _build_user_is_visitor {
-    my $self = shift;
-    my $visitor = $self->rest->user;
-    my $group = $self->group;
-
-    # visitor is a user related to this group by some account that is doing
-    # some self-join related activity.
-
-    return if $group->has_user($visitor);
-    return if $group->permission_set ne 'self-join';
-    return $self->user_is_related;
-}
+with 'Socialtext::Rest::ForGroup';
 
 # Anybody can see these, since they are just the list of workspaces the user
 # has 'selected'.
