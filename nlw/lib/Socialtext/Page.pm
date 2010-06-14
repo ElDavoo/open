8011557751bd99b5272c83c5f797205810fb00d5
Ken Pier <ken.pier@socialtext.com>
2010-06-14 11:55:36 -0700

    Removed warn statement

--- nlw/lib/Socialtext/Page.pm
+++ nlw/lib/Socialtext/Page.pm
@@ -782,7 +782,6 @@ sub add_tags {
         my $meta = $self->metadata;
         my %tags_added;
         foreach my $tag (@tags) {
-            warn $tag;
             my $added = $meta->add_category($tag);
             $tags_added{$tag} = 1;
         }
