32944b2ec17af685ad86ec6d6f67d4246a83e787
Stash <jeremy.stashewsky@socialtext.com>
2010-06-18 10:44:56 -0700

    Compensate for a bug in Socialtext::WikiText WRT a-phrases.
    The "array of regexes" grammar rules don't quite work how we want in the base
    class; we don't want "whichever regex matches first in the array" but
    "whichever regex in the array matches closest to the front of the input
    string".  Fortunately, we can provide a `match_a` method or single regex that
    would do this.  Since luke and I couldn't figure out a single regex that
    *also* retained the $1 and $2 captures, we chose to use the method instead.

--- nlw/lib/Socialtext/WikiText/Parser/Messages.pm
+++ nlw/lib/Socialtext/WikiText/Parser/Messages.pm
@@ -7,10 +7,15 @@ use base 'WikiText::Socialtext::Parser';
 
 use Socialtext::String ();
 
+my $url_scheme = qr{(?:http|https|ftp|irc|file):(?://)?};
 my $reserved   = q{;/?:@&=+$,[]#};
 my $mark       = q{-_.!~*'()};
 my $unreserved = "A-Za-z0-9\Q$mark\E";
 my $uric       = quotemeta($reserved) . $unreserved . "%";
+
+my $url_re      = qr/$url_scheme [$uric]+/x;
+my $simple_re   = qr/() (?<![<"]) ($url_re) (?![>"])/x;
+my $labelled_re = qr/(?:"([^"]*)"\s*)? < ($url_re) >/x;
 
 sub create_grammar {
     my $self = shift;
@@ -49,11 +54,8 @@ sub create_grammar {
         },
     };
 
-    my $url_scheme = qr{(?:http|https|ftp|irc|file):(?://)?};
-    $grammar->{a}{match} = [
-        qr{(?:"([^"]*)"\s*)? < ( $url_scheme [$uric ]+ ) >}x,
-        qr{()( $url_scheme [$uric]+ )}x,
-    ];
+    # see sub match_a below.
+    delete $grammar->{a}{match};
 
     $grammar->{asis}{filter} = sub {
         my $node = shift;
@@ -61,6 +63,55 @@ sub create_grammar {
     };
 
     return $grammar;
+}
+
+sub match_a {
+    my $self = shift;
+
+    unless ($self->{_checked_url_re}++) {
+        if ($self->{input} !~ $url_re) {
+            # There are no URLs in this message, disable this function by
+            # providing a defined, yet "match nothing" grammar rule.
+            $self->{grammar}{a}{match} = qr/(?!)/;
+            return;
+        }
+    }
+
+    my ($simple, $labelled);
+    if ($self->{input} =~ $simple_re) {
+#         warn "FOUND SIMPLE:   '$1'<$2>\n";
+        $simple = $-[0];
+    }
+
+    if ($self->{input} =~ $labelled_re) {
+#         warn "FOUND LABELLED: '$1'<$2>\n";
+        $labelled = $-[0];
+    }
+
+    # Calling matched_phrase expects $1, $2, $3, $-[0] and $+[0] to be set so
+    # make sure that the one we want (simple or labelled) is the last-run
+    # regexp.  The one we want is the *closest* match, preferring labelled
+    # (if we have "http://foo.com"<http://foo.com> that should be one and not
+    # two a-phrases)
+
+    my $i;
+    if (defined $simple and defined $labelled) {
+        $i = ($labelled < $simple) ? 1 : 0;
+    }
+    elsif (defined $simple) {
+        $i = 0;
+    }
+    elsif (defined $labelled) {
+        $i = 1;
+    }
+    else {
+        return;
+    }
+
+#     warn "labelled: $labelled, simple: $simple, choice: ".($i==0 ? 'simple' : 'labelled') ."\n";
+    $self->{input} =~ (($i == 0) ? $simple_re : $labelled_re);
+#     warn "FINAL: '$1' '$2'\n";
+    return $self->matched_phrase;
 }
 
 sub re_huggy {
