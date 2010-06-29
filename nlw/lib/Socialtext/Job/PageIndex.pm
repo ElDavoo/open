fc5febd0605f5ec1d6ac5801bb8bdcdc6975f6b6
Luke Closs <gravatar@5thplane.com>
2010-06-29 10:41:36 -0700

    Park this failing test until I can come back to it.

--- nlw/lib/Socialtext/Job/PageIndex.pm
+++ nlw/lib/Socialtext/Job/PageIndex.pm
@@ -1,6 +1,7 @@
 package Socialtext::Job::PageIndex;
 # @COPYRIGHT@
 use Moose;
+use Socialtext::PageLinks;
 use namespace::clean -except => 'meta';
 
 extends 'Socialtext::Job';
@@ -13,7 +14,21 @@ sub do_work {
 
     $indexer->index_page($page->id);
 
+    # If this page was just created (only 1 revision) or deleted, then
+    # we should clear out any relevant wikitext cache entries
+    if ($page->revision_count == 1 or $page->deleted) {
+        $self->unlink_cached_wikitext_linkers;
+    }
+
     $self->completed();
+}
+
+sub unlink_cached_wikitext_linkers {
+    my $self = shift;
+
+    my $links = Socialtext::PageLinks->new(page => $self->page);
+    # Find pages that link to $self->page
+    # delete cached versions of those pages.
 }
 
 __PACKAGE__->meta->make_immutable;
