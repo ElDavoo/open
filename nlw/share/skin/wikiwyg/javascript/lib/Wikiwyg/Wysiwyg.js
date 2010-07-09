ElDavoo/open

commit 72f08085c008004321950de89d8a1024f64da03b

Try to use the range's startContainer node if possible - fixes table editing bugs in safari

nlw/share/skin/wikiwyg/javascript/lib/Wikiwyg/Wysiwyg.js
@@ -1364,6 +1364,13 @@ proto.deselect = function() {
 proto.find_table_cell_with_cursor = function() {
     var doc = this.get_edit_document();
 
+    try {
+        var container = this.get_edit_window().getSelection()
+            .getRangeAt(0).startContainer;
+    }
+    catch (e) {}
+    if (container) return $(container).parents('td');
+
     jQuery("span.find-cursor", doc).removeClass('find-cursor');
 
     // Note that we explicitly don't call set_focus() here, otherwise

commit 7bcdfdaef9b6e0d97f941ed65ba4565068f6b7be

* 72f0808 accidentally broke "Insert Table"; fix that, and also make   this.deselect() run consistently for Safari.

nlw/share/skin/wikiwyg/javascript/lib/Wikiwyg/Wysiwyg.js
@@ -1368,8 +1368,14 @@ proto.find_table_cell_with_cursor = function() {
         var container = this.get_edit_window().getSelection()
             .getRangeAt(0).startContainer;
     }
-    catch (e) {}
-    if (container) return $(container).parents('td');
+    catch (e) {};
+
+    if (container) {
+        this.deselect();
+        var $td = $(container).parents('td:first');
+        if (! $td.length) { return; }
+        return $td;
+    }
 
     jQuery("span.find-cursor", doc).removeClass('find-cursor');
 
@@ -1379,8 +1385,7 @@ proto.find_table_cell_with_cursor = function() {
     this.insert_html("<span class=\"find-cursor\"></span>");
 
     var cursor = jQuery("span.find-cursor", doc);
-    if (! cursor.parents('td').length)
-        return;
+    if (! cursor.parents('td').length) { return; }
     var $cell = cursor.parents("td");
 
     cursor.remove();