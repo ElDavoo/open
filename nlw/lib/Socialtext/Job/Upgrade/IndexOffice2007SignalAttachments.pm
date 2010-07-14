017eda9b7c00df68f9a619d166f72fb282afda18
Brandon Noard <brandon.noard@socialtext.com>
2010-07-14 13:10:23 -0700

    Migration job to index Office 2007 Signal Attachments.
    Finds all of the Signals that have an Office 2007 document attached to them,
    and creates SignalIndex jobs to force the Signals to get re-indexed.  Jobs
    created at a lower priority so as to now flood/stall the Ceq queue.

---
+++ nlw/lib/Socialtext/Job/Upgrade/IndexOffice2007SignalAttachments.pm
@@ -0,0 +1,37 @@
+package Socialtext::Job::Upgrade::IndexOffice2007SignalAttachments;
+# @COPYRIGHT@
+use Moose;
+use Socialtext::JobCreator;
+use Socialtext::SQL qw(sql_execute);
+use namespace::clean -except => 'meta';
+
+extends 'Socialtext::Job';
+
+sub do_work {
+    my $self = shift;
+
+    my $sig_list = $self->_signal_ids();
+    foreach my $signal_id (@{$sig_list}) {
+        Socialtext::JobCreator->index_signal($signal_id, priority => 54);
+    }
+
+    $self->completed();
+}
+
+sub _signal_ids {
+    my $self = shift;
+    my $sth  = sql_execute( q{
+        SELECT signal_id
+          FROM signal_attachment
+          JOIN attachment USING (attachment_id)
+         WHERE attachment.filename ~* '\.(xls|doc|ppt)x$'
+           AND NOT attachment.is_temporary;
+    } );
+
+    my $rows = $sth->fetchall_arrayref();
+    my @ids  = map { $_->[0] } @{$rows};
+    return \@ids;
+}
+
+__PACKAGE__->meta->make_immutable;
+1;
