996383adfe820ca82e9ca8bba505abf5322f4786
Luke Closs <gravatar@5thplane.com>
2010-08-06 15:13:59 -0700

    Add code that switches to solr and emails people

--- nlw/lib/Socialtext/Job/Upgrade/SwitchToSolr.pm
+++ nlw/lib/Socialtext/Job/Upgrade/SwitchToSolr.pm
@@ -3,6 +3,9 @@ package Socialtext::Job::Upgrade::SwitchToSolr;
 use Moose;
 use Socialtext::JobCreator;
 use Socialtext::Log qw/st_log/;
+use Socialtext::AppConfig;
+use Socialtext::User;
+use Socialtext::EmailSender::Factory;
 use Clone qw/clone/;
 use namespace::clean -except => 'meta';
 
@@ -18,33 +21,66 @@ sub do_work {
     # find the count of PageReIndex and AttachmentReIndex jobs
     my $jobs = Socialtext::Jobs->new;
     my $count = 0;
-    for qw(Page Attachment) {
-        $count += $jobs->job_count("Socialtext::Job::${_}ReIndex");
+    for my $type (qw(PageReIndex AttachmentReIndex Upgrade::ReIndexWorkspace)) {
+        $count += $jobs->job_count("Socialtext::Job::$type");
     }
     if ($count) {
-        st_log->info("There are $count re-index jobs remaining.");
+        st_log->info(
+            "SEARCH UPGRADE: There are $count re-index jobs remaining.");
+
         my @clone_args = map { $_ => $self->job->$_ }
             qw(funcid funcname priority uniqkey coalesce);
         my $next_job = TheSchwartz::Moosified::Job->new({
             @clone_args,
             run_after => time + $Job_delay,
             arg => {
-                clone($self->arg),
+                %{clone($self->arg)},
                 last_count => $count,
             }
         });
         $self->replace_with($next_job);
     }
     else {
-        st_log->info("There are no more re-index jobs. Enabling Solr for "
-            "workspace search now.");
+        st_log->info("SEARCH UPGRADE: ".
+            "There are no more re-index jobs. ".
+            "Enabling Solr for workspace search now.");
         $self->_enable_solr();
+        $self->_email_technical_admins();
         $self->completed();
     }
 }
 
 sub _enable_solr {
     my $self = shift;
+
+    Socialtext::AppConfig->set(
+        search_factory_class => 'Socialtext::Search::Solr::Factory',
+    );
+    Socialtext::AppConfig->write;
+}
+
+sub _email_technical_admins {
+    my $self = shift;
+
+    my $admin_cursor = Socialtext::User->AllTechnicalAdmins;
+
+    my $email_sender = Socialtext::EmailSender::Factory->create('en');
+    $email_sender->send(
+        from      => Socialtext::User->SystemUser->email_address,
+        to        => [
+            map { $_->name_and_email } $admin_cursor->all()
+        ],
+        subject   => "Your Socialtext Appliance search migration is complete",
+        text_body => <<EOT,
+Greetings Human,
+
+The search re-indexing of the workspace content in your Socialtext Appliance
+has finished. Your appliance is now using the upgraded search capability.
+
+Sincerely,
+Your Socialtext Appliance
+EOT
+    );
 }
 
 __PACKAGE__->meta->make_immutable;
