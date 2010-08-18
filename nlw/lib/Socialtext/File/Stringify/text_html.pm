bec6ee025786a451955ff7a913aa0782ef28c065
Stash <jeremy.stashewsky@socialtext.com>
2010-08-17 23:43:10 -0700

    Re-implement HTML stringifier in HTML::Parser

--- nlw/lib/Socialtext/File/Stringify/text_html.pm
+++ nlw/lib/Socialtext/File/Stringify/text_html.pm
@@ -1,27 +1,162 @@
 # @COPYRIGHT@
 package Socialtext::File::Stringify::text_html;
-use strict;
 use warnings;
+use strict;
 
 use Socialtext::File::Stringify::Default;
-use Socialtext::System;
+use Socialtext::System ();
+use HTML::Parser ();
+use HTML::HeadParser ();
+use HTML::Entities qw/decode_entities/;
+use File::Temp qw/tempfile/;
+use Encode ();
+use POSIX ();
+use Guard;
+use List::MoreUtils qw/firstidx/;
 
 sub to_string {
-    my ( $class, $buf_ref, $file, $mime ) = @_;
-    my @cmd = "lynx";
-    push @cmd, '-dump' => $file;
-    Socialtext::System::backtick(@cmd, {stdout => $buf_ref});
-    Socialtext::File::Stringify::Default->to_string($buf_ref, $file, $mime)
-        if $? or $@;
+    my ( $class, $buf_ref, $filename, $mime ) = @_;
+
+    return if $class->stringify_html($buf_ref, $filename, $mime);
+    Socialtext::File::Stringify::Default->to_string($buf_ref,$filename,$mime);
+    return;
+}
+
+our $temp_fh;
+our $temp_filename;
+our $enc;
+
+sub stringify_html {
+    my ( $class, $buf_ref, $filename, $mime ) = @_;
+
+    my ($charset) = ($mime =~ /charset=(.+);?/);
+
+    my ($tfh, $tfilename) = tempfile(
+        "/tmp/htmlstringify-$$-XXXXXX", CLEANUP => 1);
+    local $temp_fh = $tfh;
+    local $temp_filename = $tfilename;
+    local $enc;
+
+    binmode $temp_fh, ':utf8';
+
+#     my $pid = fork;
+#     die "can't fork html stringifier: $!" unless defined $pid;
+#     if ($pid) { # parent
+#         my $g = guard { kill -9, $pid };
+#         # TODO: alarm
+#         eval {
+#             local $SIG{ALRM} = sub { die 'timeout' };
+#             alarm 15;
+#             waitpid $pid, 0;
+#             alarm 0;
+#         };
+#         my $rv = $? >> 8;
+#         if ($rv) {
+#             $@ = "HTML stringifier crashed $@";
+#             return;
+#         }
+#         else {
+#             $g->cancel;
+#         }
+#     }
+#     else { # kid
+#         # TODO: limit memory
+#         eval { _run_stringifier($filename, $charset) };
+#         my $e = $@;
+#         warn $@ if $@;
+#         $temp_fh->flush;
+#         close $temp_fh; # flush
+#         my $f = $!;
+#         my $g;
+#         if ($e || $f) {
+#             unlink $temp_filename;
+#             $g = $!;
+#         }
+#         POSIX::_exit($e||$f||$g ? 1 : 0);
+#     }
+
+    _run_stringifier($filename, $charset);
+
+    seek $temp_fh, 0, 0; # rewind
+    binmode $temp_fh, ':mmap'; # turn utf8 layer off so we can ...
+    $$buf_ref = do { local $/; <$temp_fh> }; # ... slurp with max efficiency!
+    # And because it just wrote the file as utf8 so we can safely just switch
+    # the flag on.
+    Encode::_utf8_on($$buf_ref);
+
+    return 1;
+}
+
+sub _run_stringifier {
+    my ($filename, $charset) = @_;
+    $charset = _detect_charset($filename) unless $charset;
+    $enc = Encode::find_encoding($charset);
+    my $p = HTML::Parser->new(
+        ignore_elements => [qw(style script)],
+        text_h => [\&_got_text, 'text'],
+        start_h => [\&_got_start, 'tagname, @attr'],
+        utf8_mode => 0,
+        attr_encoded => 1, # do our own decoding of attr entities
+        case_sensitive => 0, # lowercase tags and attrs
+    );
+    $p->parse_file($filename);
+    return;
+}
+
+sub _detect_charset {
+    my $filename = shift;
+    my $hp = HTML::HeadParser->new();
+    $hp->parse_file($filename);
+    my $charset = $hp->header('Content-Type');
+    if ($charset) {
+        my ($cs) = ($charset =~ /charset=(.+);?/);
+        $charset = $cs ? $cs : undef;
+    }
+    else {
+        $charset = $hp->header('X-Meta-Charset');
+    }
+    $charset ||= 'UTF-8';
+    return $charset;
+}
+
+sub _got_text {
+    my $t = shift;
+    $t = $enc->decode($t);
+    decode_entities($t);
+    $t =~ s/\s+/ /smg;
+    return if ($t eq ' ' || $t eq '');
+    print $temp_fh $t, ' ';
+}
+
+sub _got_start {
+    my $tag = shift;
+    my $in;
+    if ($tag eq 'a') {
+        my $i = firstidx { $_ eq 'href' } @_;
+        $in = $_[$i+1] if ($i >= 0 && $i <= $#_);
+    }
+    elsif ($tag eq 'meta') {
+        my %attr = @_;
+        if (my $name = $attr{name}) {
+            $in = $attr{content}
+                if ($name eq 'keywords' or $name eq 'description');
+        }
+    }
+
+    return unless defined $in;
+    my $out = $enc->decode($in);
+    decode_entities($out);
+    print $temp_fh $out, ' ';
 }
 
 1;
+__END__
 
 =head1 NAME
 
 Socialtext::File::Stringify::text_html - Stringify HTML documents
 
-=head1 METHODS
+=head1 CLASS METHODS
 
 =over
 
@@ -37,6 +172,6 @@ Socialtext, Inc. C<< <code@socialtext.com> >>
 
 =head1 COPYRIGHT
 
-Copyright 2006 Socialtext, Inc., all rights reserved.
+Copyright 2006-2010 Socialtext, Inc., all rights reserved.
 
 =cut
---
+++ nlw/t/Socialtext/File/Stringify-html.t
@@ -0,0 +1,112 @@
+#!perl
+use warnings;
+use strict;
+use Test::More tests => 58;
+use Test::Exception;
+use utf8;
+
+use ok 'Socialtext::File::Stringify::text_html';
+
+my $base_dir = 't/Socialtext/File/stringify_data/html';
+
+sub to_str { Socialtext::File::Stringify::text_html->to_string(@_) };
+
+sub has_japanese_content ($$;$) {
+    my ($buf, $ct, $name) = @_;
+    $name ||= $ct;
+    local $Test::Builder::Level = $Test::Builder::Level + 1;
+    ok $buf =~ /\Q日本語/, "$name found decoded title"; # "Japanese"
+    ok $buf =~ /\Qキーワード/, "$name found meta keywords"; # "keywords"
+    ok $buf =~ /\Q説明/, "$name found meta description"; # "description"
+    ok $buf =~ /\Qこのページは${ct}でいる/,
+        "$name body text"; # "this page is $charset"
+    ok $buf =~ m#\Qhttp://socialtext.com/?$ct#i, "$name a-tag link href";
+    ok $buf =~ /\Qリンクテキスト/, "$name a-tag link text"; # "link text"
+}
+
+sub has_danish_content ($$;$) {
+    my ($buf, $ct, $name) = @_;
+    $name ||= $ct;
+    local $Test::Builder::Level = $Test::Builder::Level + 1;
+    ok $buf =~ /\QDansk/, "$name decoded title";
+    ok $buf =~ /\Qsøgeord/, "$name meta keywords"; # "keywords"
+    ok $buf =~ /\Qbeskrivelse/, "$name meta description"; # "description"
+    like $buf, qr/\Qdenne side er $ct. Her er et billede af en kanin med en pandekage på hovedet. Æ!/, "$name body text"; # "this page is $charset. Here is a picture of a rabbit with a pancake on it's head. Ae!"
+    ok $buf =~ m#\Qhttp://socialtext.com/?$ct#i, "$name a-tag link href";
+    ok $buf =~ /\Qlinktekst/, "$name a-tag link text"; # "link text"
+}
+
+utf8: {
+    my $filename = $base_dir .'/japanese-utf8.html';
+    my $buf;
+    lives_ok {
+        to_str(\$buf, $filename, 'text/html; charset=UTF-8');
+    } 'stringify with explicit charset';
+    has_japanese_content($buf, 'UTF-8');
+}
+
+utf8_with_guess: {
+    my $filename = $base_dir .'/japanese-utf8.html';
+    my $buf;
+    lives_ok {
+        to_str(\$buf, $filename, 'text/html');
+    } 'stringify with absent charset (derived by meta header), guess utf8';
+    has_japanese_content($buf, 'UTF-8', "UTF-8-guessed");
+}
+
+utf16_no_guess: {
+    my $filename = $base_dir .'/japanese-utf16.html';
+    my $buf;
+    lives_ok {
+        to_str(\$buf, $filename, 'text/html; charset=UTF-16LE');
+    } 'stringify with explicit UTF-16LE charset';
+    has_japanese_content($buf, 'UTF-16', "UTF-16LE");
+}
+
+utf16_guess: {
+    my $filename = $base_dir .'/japanese-utf16.html';
+    my $buf;
+    lives_ok {
+        to_str(\$buf, $filename, 'text/html');
+    } 'stringify UTF-16 with absent charset (derived by meta header)';
+    local $TODO = 'UTF-16LE guessing not implemented';
+    has_japanese_content($buf, 'UTF-16', "UTF-16LE-guessed");
+}
+
+sjis: {
+    my $filename = $base_dir .'/japanese-shiftjis.html';
+    my $buf;
+    lives_ok {
+        to_str(\$buf, $filename, 'text/html; charset=Shift_JIS');
+    } 'stringify with explicit Shift_JIS charset';
+    has_japanese_content($buf, 'Shift-JIS', 'Shift_JIS');
+}
+
+sjis_with_guess: {
+    my $filename = $base_dir .'/japanese-shiftjis.html';
+    my $buf;
+    lives_ok {
+        to_str(\$buf, $filename, 'text/html');
+    } 'stringify with absent charset (derived by meta header), guess sjis';
+    has_japanese_content $buf, 'Shift-JIS', 'Shift_JIS-guessed';
+}
+
+danish_utf8_with_guess: {
+    my $filename = $base_dir .'/danish-utf8.html';
+    my $buf;
+    lives_ok {
+        to_str(\$buf, $filename, 'text/html');
+    } 'stringify with absent charset (derived by meta header), guess utf8';
+    has_danish_content($buf, 'UTF-8', "UTF-8-danish");
+}
+
+danish_iso_8859_1_with_guess: {
+    my $filename = $base_dir .'/danish-iso-8859-1.html';
+    my $buf;
+    lives_ok {
+        to_str(\$buf, $filename, 'text/html');
+    } 'stringify with absent charset (derived by meta header), guess iso';
+    has_danish_content($buf, 'ISO-8859-1', "ISO-8859-1-danish");
+}
+
+pass 'done';
