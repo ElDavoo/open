9cc3925330e55fa47ee23afd4bd4a403e2a62a5a
Graham TerMarsch <graham.termarsch@socialtext.com>
2010-09-20 21:55:41 -0700

    Helper methods to create cookies that need to be renewed or that have already expired.

--- nlw/lib/Test/Socialtext/Cookie.pm
+++ nlw/lib/Test/Socialtext/Cookie.pm
@@ -13,6 +13,28 @@ sub BuildCookie {
     return join '=', $name, $value;
 }
 
+sub BuildCookieNeedingRenewal {
+    my $class = shift;
+    my $now   = time;
+    return $class->BuildCookie(
+        @_,
+        'not-before'      => ($now - 3600),
+        'renew-until'     => ($now - 1800),
+        'not-on-or-after' => ($now + 86400),
+    );
+}
+
+sub BuildExpiredCookie {
+    my $class = shift;
+    my $now   = time;
+    return $class->BuildCookie(
+        @_,
+        'not-before'      => ($now - 3600),
+        'renew-until'     => ($now - 1800),
+        'not-on-or-after' => ($now -  300),
+    );
+}
+
 1;
 
 =head1 NAME
@@ -25,6 +47,16 @@ Test::Socialtext::Cookie - methods to manipulate cookies within tests
 
   # create a new Cookie (as a string)
   $cookie = Test::Socialtext::Cookie->BuildCookie(user_id => $user_id);
+
+  # create a Cookie that needs to be renewed
+  $cookie = Test::Socialtext::Cookie->BuildCookieNeedingRenewal(
+      user_id => $user_id,
+  );
+
+  # create a Cookie that has already expired
+  $cookie = Test::Socialtext::Cookie->BuildExpiredCookie(
+      user_id => $user_id,
+  );
 
 =head1 DESCRIPTION
 
@@ -39,6 +71,17 @@ cookies from within test suites.
 
 Creates a new cookie based on the provided C<%values>, returning that cookie
 back to the caller as a string.
+
+=item B<Test::Socialtext::Cookie-E<gt>BuildCookieNeedingRenewal(%values)>
+
+Creates a new cookie based on the provided C<%values>, that has already passed
+its "soft limit" and is in need of renewal.  Cookie returned back to the
+caller as a string.
+
+=item B<Test::Socialtext::Cookie-E<gt>BuildExpiredCookie(%values)>
+
+Creates an I<expired> cookie based on the provided C<%values>, returning that
+cookie back to the caller as a string.
 
 =back
 
