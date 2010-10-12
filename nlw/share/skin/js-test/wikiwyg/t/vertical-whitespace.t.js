808a22b761cd43ff7c44e8e7ba479a01f6fc53dd
Audrey Tang <audrey.tang@socialtext.com>
2010-10-12 04:52:01 -0700

    * Add FF version of the roundtrip.

--- nlw/share/skin/js-test/wikiwyg/t/vertical-whitespace.t.js
+++ nlw/share/skin/js-test/wikiwyg/t/vertical-whitespace.t.js
@@ -4,7 +4,7 @@ var filters = {
     html: ['html_to_wikitext']
 };
 
-t.plan(1);
+t.plan(3);
 t.filters(filters);
 t.run_is('html', 'wikitext');
 
@@ -23,5 +23,51 @@ t.run_is('html', 'wikitext');
 
 
 3
+
+=== Vertical whitespace in Firefox (part 1)
+--- html
+<div class="wiki">1<br><br><br><br>3<br><br><br><br><br>5<br></div>
+--- wikitext
+1
+
+
+
+3
+
+
+
+
+5
+
+=== Vertical whitespace in Firefox (part 2)
+--- html
+<div class="wiki">
+<p>
+1<br>
+<br>
+<br>
+<br>
+</p>
+<p>
+3<br>
+<br>
+<br>
+<br>
+<br>
+</p>
+<p>
+5</p>
+</div>
+--- wikitext
+1
+
+
+
+3
+
+
+
+
+5
 
 */
