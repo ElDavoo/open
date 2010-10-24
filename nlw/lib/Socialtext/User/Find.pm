ElDavoo/open

commit 4d24114d5b6bdb24878eb28f08dc64e98ec47a68

Cherry-pick user lookahead optimization for Getty

nlw/lib/Socialtext/User/Find.pm
@@ -100,34 +100,31 @@ sub _build_sql_where {
     # get sql/bindings for Users that are actually visible to us
     my ($vis_sql, @vis_bind);
     {
-        my $vis_from = q{
-            user_sets_for_user other
-            JOIN user_sets_for_user viewer USING (user_set_id)
+        $vis_sql = q{
+            EXISTS (
+                SELECT 1
+                FROM user_set_path other
+                WHERE other.from_set_id = users.user_id
+                  AND EXISTS (
+                    SELECT 1
+                    FROM user_set_path viewer
+                    WHERE viewer.from_set_id = ?
+                      AND other.into_set_id = viewer.into_set_id
+                  )
+            )
         };
-        my $vis_cols  = 'other.user_id';
-        my $vis_where = $self->all
-            ? { }
-            : { 'viewer.user_id' => $self->viewer->user_id };
-        ($vis_sql, @vis_bind) = sql_abstract()->select(
-            \$vis_from, $vis_cols, $vis_where,
-        );
+        @vis_bind = ($self->viewer->user_id);
     }
 
-    my @in_visible_users = (
-        user_id => { -in => \[$vis_sql, @vis_bind] },
-    );
-
     return {
-        (
-            '-and' => [ @in_visible_users ],
-            '-or'  => [
-                'lower(first_name)'      => { '-like' => $filter },
-                'lower(last_name)'       => { '-like' => $filter },
-                'lower(email_address)'   => { '-like' => $filter },
-                'lower(driver_username)' => { '-like' => $filter },
-                'lower(display_name)'    => { '-like' => $filter },
-            ],
-        )
+        '-nest' => \[ $vis_sql, @vis_bind ],
+        '-or'  => [
+            'lower(first_name)'      => { '-like' => $filter },
+            'lower(last_name)'       => { '-like' => $filter },
+            'lower(email_address)'   => { '-like' => $filter },
+            'lower(driver_username)' => { '-like' => $filter },
+            'lower(display_name)'    => { '-like' => $filter },
+        ],
     };
 }
 

commit 71087337f7b92b8e5871965683429210f7ec8be3

* Fix /data/users?all=1 support accidentally broken by 4d24114d.

nlw/lib/Socialtext/User/Find.pm
@@ -97,9 +97,19 @@ sub _build_sql_where {
     my $self = shift;
     my $filter = $self->filter;
 
+    my %where = (
+        '-or'  => [
+            'lower(first_name)'      => { '-like' => $filter },
+            'lower(last_name)'       => { '-like' => $filter },
+            'lower(email_address)'   => { '-like' => $filter },
+            'lower(driver_username)' => { '-like' => $filter },
+            'lower(display_name)'    => { '-like' => $filter },
+        ],
+    );
+
     # get sql/bindings for Users that are actually visible to us
-    my ($vis_sql, @vis_bind);
-    {
+    if (!$self->all) {
+        my ($vis_sql, @vis_bind);
         $vis_sql = q{
             EXISTS (
                 SELECT 1
@@ -114,18 +124,10 @@ sub _build_sql_where {
             )
         };
         @vis_bind = ($self->viewer->user_id);
+        $where{'-nest'} = \[ $vis_sql, @vis_bind ],
     }
 
-    return {
-        '-nest' => \[ $vis_sql, @vis_bind ],
-        '-or'  => [
-            'lower(first_name)'      => { '-like' => $filter },
-            'lower(last_name)'       => { '-like' => $filter },
-            'lower(email_address)'   => { '-like' => $filter },
-            'lower(driver_username)' => { '-like' => $filter },
-            'lower(display_name)'    => { '-like' => $filter },
-        ],
-    };
+    return \%where;
 }
 
 has 'sql_order' => ( is => 'ro', isa => 'HashRef', lazy_build => 1 );