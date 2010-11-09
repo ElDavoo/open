16784f8bc3cc7b9a160445eb83cc326ecc4560f3
Luke Closs <luke.closs@socialtext.com>
2010-11-09 14:28:34 -0800

    Bulk insert GroupReIndex jobs for speed

--- nlw/lib/Socialtext/Group.pm
+++ nlw/lib/Socialtext/Group.pm
@@ -502,14 +502,25 @@ sub IndexGroups {
     }
 
     sql_begin_work();
-    my $all_groups = sql_execute('SELECT group_id FROM groups');
-    my $i = 0;
-    while ( my ($group_id) = $all_groups->fetchrow_array ) {
-        Socialtext::JobCreator->index_group($group_id);
-        $i++;
+    my $sth = sql_execute('SELECT group_id FROM groups');
+    my @jobs;
+    while (my ($group_id) = $sth->fetchrow_array) {
+        push @jobs, {
+            coalesce => "$group_id-reindex", # don't coalesce with normal jobs
+            arg => $group_id,
+        };
     }
+
+    st_log()->info("going to insert ".scalar(@jobs)." GroupReIndex jobs");
+    my $template_job = TheSchwartz::Moosified::Job->new(
+        funcname => 'Socialtext::Job::GroupReIndex',
+        priority => -30,
+    );
+    Socialtext::JobCreator->bulk_insert($template_job, \@jobs);
+    st_log()->info("done GroupReIndex bulk_insert");
+
     sql_commit();
-    return $i;
+    return scalar(@jobs);
 }
 
 ###############################################################################
