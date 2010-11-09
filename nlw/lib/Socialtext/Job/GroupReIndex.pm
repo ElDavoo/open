513c3d752278738aafff5a2fcb365d794116f00c
Luke Closs <luke.closs@socialtext.com>
2010-11-09 14:24:50 -0800

    Add ReIndex jobs for Person and Group objects
    * ReIndex jobs do not force a solr commit, they let the background
    solr autocommit do the work.  This makes indexing much faster.

--- nlw/lib/Socialtext/Job/GroupReIndex.pm
+++ nlw/lib/Socialtext/Job/GroupReIndex.pm
@@ -1,9 +1,9 @@
-package Socialtext::Job::SignalReIndex;
+package Socialtext::Job::GroupReIndex;
 # @COPYRIGHT@
 use Moose;
 use namespace::clean -except => 'meta';
 
-extends 'Socialtext::Job::SignalIndex';
+extends 'Socialtext::Job::GroupIndex';
 with 'Socialtext::ReIndexJob';
 
 __PACKAGE__->meta->make_immutable;
@@ -11,20 +11,20 @@ __PACKAGE__->meta->make_immutable;
 
 =head1 NAME
 
-Socialtext::Job::SignalReIndex - An alias for Socialtext::Job::SignalIndex
+Socialtext::Job::GroupReIndex - An alias for Socialtext::Job::GroupIndex
 
 =head1 SYNOPSIS
 
     use Socialtext::JobCreator;
 
     Socialtext::JobCreator->insert(
-        'Socialtext::Job::SignalReIndex => { %args }
+        'Socialtext::Job::GroupReIndex => { %args }
     );
 
 =head1 DESCRIPTION
 
-Similar to Socialtext::Job::SignalIndex. In the special case where we need to
+Similar to Socialtext::Job::GroupIndex. In the special case where we need to
 know when we're RE-indexing, eg, when pushing an update to our DB/search
-engine layout, SignalReIndex should be used.
+engine layout, GroupReIndex should be used.
 
 =cut
--- nlw/lib/Socialtext/Job/PersonReIndex.pm
+++ nlw/lib/Socialtext/Job/PersonReIndex.pm
@@ -1,9 +1,9 @@
-package Socialtext::Job::SignalReIndex;
+package Socialtext::Job::PersonReIndex;
 # @COPYRIGHT@
 use Moose;
 use namespace::clean -except => 'meta';
 
-extends 'Socialtext::Job::SignalIndex';
+extends 'Socialtext::Job::PersonIndex';
 with 'Socialtext::ReIndexJob';
 
 __PACKAGE__->meta->make_immutable;
@@ -11,20 +11,20 @@ __PACKAGE__->meta->make_immutable;
 
 =head1 NAME
 
-Socialtext::Job::SignalReIndex - An alias for Socialtext::Job::SignalIndex
+Socialtext::Job::PersonReIndex - An alias for Socialtext::Job::PersonIndex
 
 =head1 SYNOPSIS
 
     use Socialtext::JobCreator;
 
     Socialtext::JobCreator->insert(
-        'Socialtext::Job::SignalReIndex => { %args }
+        'Socialtext::Job::PersonReIndex => { %args }
     );
 
 =head1 DESCRIPTION
 
-Similar to Socialtext::Job::SignalIndex. In the special case where we need to
+Similar to Socialtext::Job::PersonIndex. In the special case where we need to
 know when we're RE-indexing, eg, when pushing an update to our DB/search
-engine layout, SignalReIndex should be used.
+engine layout, PersonReIndex should be used.
 
 =cut
