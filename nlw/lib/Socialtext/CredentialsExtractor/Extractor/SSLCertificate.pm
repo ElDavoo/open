73da9e5177d29042a18650f3be960d75805e0295
Graham TerMarsch <graham.termarsch@socialtext.com>
2010-11-23 15:06:08 -0800

    Refactor, splitting out the subject exploding to a separate sub.

--- nlw/lib/Socialtext/CredentialsExtractor/Extractor/SSLCertificate.pm
+++ nlw/lib/Socialtext/CredentialsExtractor/Extractor/SSLCertificate.pm
@@ -14,16 +14,7 @@ sub uses_headers {
 sub extract_credentials {
     my ($class, $hdrs) = @_;
 
-    # Get the Subject DN from the headers, and normalize its format.
-    my $subject = $hdrs->{X_SSL_CLIENT_SUBJECT};
-    $subject =~ s{^/\s*}{}g;        # eliminate leading '/'s
-    $subject =~ s{/\s*}{, }g;       # convert '/'s to ','s
-    # XXX: doesn't accommodate "..., ..., .../..." (embedded slashes)
-
-    # Split the subject up into its component fields.
-    my $fields = ldap_explode_dn($subject);
-
-    # Grab the field that contains the Username in it
+    my $fields     = $class->_explode_subject($hdrs->{X_SSL_CLIENT_SUBJECT});
     my $user_field = 'CN';
     my ($username) =
         map  { $_->{$user_field} }
@@ -33,6 +24,18 @@ sub extract_credentials {
     my $user_id = $class->username_to_user_id($username);
     return $class->valid_creds(user_id => $user_id) if ($user_id);
     return $class->invalid_creds(reason => "invalid username: $username");
+}
+
+sub _explode_subject {
+    my $class   = shift;
+    my $subject = shift;
+    $subject =~ s{^/\s*}{}g;        # eliminate leading '/'s
+    $subject =~ s{/\s*}{, }g;       # convert '/'s to ','s
+    # XXX: doesn't accommodate "..., ..., .../..." (embedded slashes)
+
+    # Split the subject up into its component fields.
+    my $fields = ldap_explode_dn($subject);
+    return $fields;
 }
 
 no Moose;
