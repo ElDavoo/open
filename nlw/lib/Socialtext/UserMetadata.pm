e4d807665eae97e5449c0fdb599bb3cf14d0089a
Stash <jeremy.stashewsky@socialtext.com>
2010-11-24 10:11:23 -0800

    UserMetadata defines its own constructor

--- nlw/lib/Socialtext/UserMetadata.pm
+++ nlw/lib/Socialtext/UserMetadata.pm
@@ -237,7 +237,7 @@ sub _validate_and_clean_data {
     data_validation_error errors => \@errors if @errors;
 }
 
-__PACKAGE__->meta->make_immutable;
+__PACKAGE__->meta->make_immutable(inline_constructor => 0);
 1;
 
 __END__
