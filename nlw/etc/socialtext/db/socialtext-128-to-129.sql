5e1adf057bd749dfd2eaf2fc381bc9d41edc266a
Kevin Jones <kevin.jones@socialtext.com>
2010-12-03 10:33:55 -0800

    Fix {bz: 4723} - drop the primary key on gallery while we change gallery ids

--- nlw/etc/socialtext/db/socialtext-128-to-129.sql
+++ nlw/etc/socialtext/db/socialtext-128-to-129.sql
@@ -14,6 +14,10 @@ ALTER TABLE gallery_gadget
     -- wrong name anyway, really this should be gallery_gadget_gallery_id_fk
     DROP CONSTRAINT gallery_gadget_account_fk;
 
+-- We are about to break this constraint, but we will add it back right
+-- after the migration runs
+ALTER TABLE gallery DROP CONSTRAINT gallery_pk;
+
 -- fill in the account_ids of all gallery gadgets so we can change gallery ids
 UPDATE gallery_gadget
     SET account_id = (
@@ -26,18 +30,19 @@ UPDATE gallery_gadget
 UPDATE gallery_gadget SET gallery_id = account_id WHERE account_id IS NOT NULL;
 UPDATE gallery SET gallery_id = account_id WHERE account_id IS NOT NULL;
 
+-- And create a constraint that requires the gallery_id to be 0 or equal the
+-- account_id
+ALTER TABLE gallery
+    ADD CONSTRAINT gallery_id_zero_or_account_id
+        CHECK(gallery_id = 0 OR gallery_id = account_id),
+    ADD PRIMARY KEY(gallery_id); -- add this back
+
 -- Add back the constraints, and get rid of our temporary account_id column
 ALTER TABLE gallery_gadget
     DROP COLUMN account_id,
     ADD CONSTRAINT gallery_gadget_gallery_id_fk
         FOREIGN KEY (gallery_id)
         REFERENCES gallery(gallery_id) ON DELETE CASCADE;
-
--- And create a constraint that requires the gallery_id to be 0 or equal the
--- account_id
-ALTER TABLE gallery
-    ADD CONSTRAINT gallery_id_zero_or_account_id
-        CHECK(gallery_id = 0 OR gallery_id = account_id);
 
 -- this sequence is useless now that gallery_id is just account_id or 0
 DROP SEQUENCE gallery_id;
