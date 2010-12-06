f0171dd67a1c594815698a201109d6a8f86da08b
Graham TerMarsch <graham.termarsch@socialtext.com>
2010-12-06 11:18:22 -0800

    Add missing POD.

--- nlw/lib/Socialtext/Challenger/NoLogin.pm
+++ nlw/lib/Socialtext/Challenger/NoLogin.pm
@@ -22,3 +22,45 @@ sub challenge {
 }
 
 1;
+
+=head1 NAME
+
+Socialtext::Challenger::NoLogin - Custom challenger with *NO* login form
+
+=head1 SYNOPSIS
+
+  Do not instantiate this class directly.
+  Use Socialtext::Challenger instead.
+
+=head1 DESCRIPTION
+
+When configured for use, this Challenger presents to unauthenticated Users a
+page that contains B<NO> login form.
+
+Useful when credentials are to be provided by some means I<other than> username and password (e.g. SSL Certificate).
+
+=head1 METHODS
+
+=over
+
+=item B<Socialtext::Challenger::NoLogin-E<gt>challenge(%p)>
+
+Custom challenger.
+
+Not to be called directly.  Use C<Socialtext::Challenger> instead.
+
+=back
+
+=head1 AUTHOR
+
+Socialtext, Inc. C<< <code@socialtext.com> >>
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2010 Socialtext, Inc., All Rights Reserved.
+
+=head1 SEE ALSO
+
+L<Socialtext::Challenger>.
+
+=cut
