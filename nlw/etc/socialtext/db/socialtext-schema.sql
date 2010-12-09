8883bfec23eeab4efccdf05b021aaf828e94242e
Stash <jeremy.stashewsky@socialtext.com>
2010-12-09 13:36:34 -0800

    Re-generate the schema with the updated pglist.
    This is a whitespace-only change.
    
    This needs f68ffc0723566c6c020ef79e8d8b796be6cefa78 from stconfig to be in
    place (adds a -A flag to the psql command that dumps view definitions).

--- nlw/etc/socialtext/db/socialtext-schema.sql
+++ nlw/etc/socialtext/db/socialtext-schema.sql
@@ -26,16 +26,13 @@ CREATE FUNCTION _int_contained(integer[], integer[]) RETURNS boolean
     AS '$libdir/_int', '_int_contained'
     LANGUAGE c STRICT;
 
-
 CREATE FUNCTION _int_contains(integer[], integer[]) RETURNS boolean
     AS '$libdir/_int', '_int_contains'
     LANGUAGE c STRICT;
 
-
 CREATE FUNCTION _int_different(integer[], integer[]) RETURNS boolean
     AS '$libdir/_int', '_int_different'
     LANGUAGE c STRICT;
-
 
 CREATE FUNCTION _int_inter(integer[], integer[]) RETURNS integer[]
     AS '$libdir/_int', '_int_inter'
@@ -45,11 +42,9 @@ CREATE FUNCTION _int_overlap(integer[], integer[]) RETURNS boolean
     AS '$libdir/_int', '_int_overlap'
     LANGUAGE c STRICT;
 
-
 CREATE FUNCTION _int_same(integer[], integer[]) RETURNS boolean
     AS '$libdir/_int', '_int_same'
     LANGUAGE c STRICT;
-
 
 CREATE FUNCTION _int_union(integer[], integer[]) RETURNS integer[]
     AS '$libdir/_int', '_int_union'
@@ -76,7 +71,6 @@ $$
 CREATE FUNCTION boolop(integer[], query_int) RETURNS boolean
     AS '$libdir/_int', 'boolop'
     LANGUAGE c STRICT;
-
 
 CREATE FUNCTION cleanup_sessions() RETURNS "trigger"
     AS $$
@@ -324,7 +318,6 @@ CREATE FUNCTION querytree(query_int) RETURNS text
 CREATE FUNCTION rboolop(query_int, integer[]) RETURNS boolean
     AS '$libdir/_int', 'rboolop'
     LANGUAGE c STRICT;
-
 
 CREATE FUNCTION signal_sent() RETURNS "trigger"
     AS $$
@@ -690,12 +683,12 @@ CREATE TABLE user_set_path (
 );
 
 CREATE VIEW user_sets_for_user AS
-  SELECT user_set_path.from_set_id AS user_id, user_set_path.into_set_id AS user_set_id
+ SELECT user_set_path.from_set_id AS user_id, user_set_path.into_set_id AS user_set_id
    FROM user_set_path
   WHERE user_set_path.from_set_id <= B'00010000000000000000000000000000'::"bit"::integer;
 
 CREATE VIEW accounts_for_user AS
-  SELECT user_sets_for_user.user_id, user_sets_for_user.user_set_id, user_sets_for_user.user_set_id - B'00110000000000000000000000000000'::"bit"::integer AS account_id
+ SELECT user_sets_for_user.user_id, user_sets_for_user.user_set_id, user_sets_for_user.user_set_id - B'00110000000000000000000000000000'::"bit"::integer AS account_id
    FROM user_sets_for_user
   WHERE user_sets_for_user.user_set_id >= B'00110000000000000000000000000001'::"bit"::integer AND user_sets_for_user.user_set_id <= B'01000000000000000000000000000000'::"bit"::integer;
 
@@ -916,7 +909,7 @@ CREATE SEQUENCE groups___group_id
     CACHE 1;
 
 CREATE VIEW groups_for_user AS
-  SELECT user_sets_for_user.user_id, user_sets_for_user.user_set_id, user_sets_for_user.user_set_id - B'00010000000000000000000000000000'::"bit"::integer AS group_id
+ SELECT user_sets_for_user.user_id, user_sets_for_user.user_set_id, user_sets_for_user.user_set_id - B'00010000000000000000000000000000'::"bit"::integer AS group_id
    FROM user_sets_for_user
   WHERE user_sets_for_user.user_set_id >= B'00010000000000000000000000000001'::"bit"::integer AND user_sets_for_user.user_set_id <= B'00100000000000000000000000000000'::"bit"::integer;
 
@@ -1046,7 +1039,7 @@ CREATE TABLE recent_signal_user_set (
 );
 
 CREATE VIEW roles_for_user AS
-  SELECT user_set_path.from_set_id AS user_id, user_set_path.into_set_id AS user_set_id, user_set_path.role_id
+ SELECT user_set_path.from_set_id AS user_id, user_set_path.into_set_id AS user_set_id, user_set_path.role_id
    FROM user_set_path
   WHERE user_set_path.from_set_id <= B'00010000000000000000000000000000'::"bit"::integer;
 
@@ -1173,7 +1166,7 @@ CREATE TABLE user_set_include (
 );
 
 CREATE VIEW user_set_include_tc AS
-  SELECT DISTINCT user_set_path.from_set_id, user_set_path.into_set_id, user_set_path.role_id
+ SELECT DISTINCT user_set_path.from_set_id, user_set_path.into_set_id, user_set_path.role_id
    FROM user_set_path
   ORDER BY user_set_path.from_set_id, user_set_path.into_set_id, user_set_path.role_id;
 
@@ -1195,7 +1188,7 @@ CREATE TABLE user_set_plugin_pref (
 );
 
 CREATE VIEW user_set_plugin_tc AS
-  SELECT user_set_plugin.user_set_id, user_set_plugin.plugin
+ SELECT user_set_plugin.user_set_id, user_set_plugin.plugin
    FROM user_set_plugin
 UNION ALL 
  SELECT path.from_set_id AS user_set_id, plug.plugin
@@ -1203,7 +1196,7 @@ UNION ALL
    JOIN user_set_plugin plug ON path.into_set_id = plug.user_set_id;
 
 CREATE VIEW user_use_plugin AS
-  SELECT user_set_path.from_set_id AS user_id, user_set_path.into_set_id AS user_set_id, user_set_plugin.plugin
+ SELECT user_set_path.from_set_id AS user_id, user_set_path.into_set_id AS user_set_id, user_set_plugin.plugin
    FROM user_set_path
    JOIN user_set_plugin ON user_set_path.into_set_id = user_set_plugin.user_set_id;
 
@@ -1238,13 +1231,13 @@ CREATE SEQUENCE users___user_id
     CACHE 1;
 
 CREATE VIEW users_share_plugin AS
-  SELECT v_path.user_id AS viewer_id, o_path.user_id AS other_id, v_path.user_set_id, plug.plugin
+ SELECT v_path.user_id AS viewer_id, o_path.user_id AS other_id, v_path.user_set_id, plug.plugin
    FROM user_sets_for_user v_path
    JOIN user_set_plugin plug USING (user_set_id)
    JOIN user_sets_for_user o_path USING (user_set_id);
 
 CREATE VIEW users_share_plugin_tc AS
-  SELECT v_path.user_id AS viewer_id, o_path.user_id AS other_id, v_path.user_set_id, plug.plugin
+ SELECT v_path.user_id AS viewer_id, o_path.user_id AS other_id, v_path.user_set_id, plug.plugin
    FROM user_sets_for_user v_path
    JOIN user_set_plugin_tc plug USING (user_set_id)
    JOIN user_sets_for_user o_path USING (user_set_id);
@@ -1282,7 +1275,7 @@ CREATE SEQUENCE webhook___webhook_id
     CACHE 1;
 
 CREATE VIEW workspaces_for_user AS
-  SELECT user_sets_for_user.user_id, user_sets_for_user.user_set_id, user_sets_for_user.user_set_id - B'00100000000000000000000000000000'::"bit"::integer AS workspace_id
+ SELECT user_sets_for_user.user_id, user_sets_for_user.user_set_id, user_sets_for_user.user_set_id - B'00100000000000000000000000000000'::"bit"::integer AS workspace_id
    FROM user_sets_for_user
   WHERE user_sets_for_user.user_set_id >= B'00100000000000000000000000000001'::"bit"::integer AND user_sets_for_user.user_set_id <= B'00110000000000000000000000000000'::"bit"::integer;
 
