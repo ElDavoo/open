c7a89fcb02d2ce5e314641cbb58d0eae2ec1fea3
swdevlin <shawn.devlin@socialtext.com>
2010-12-09 14:16:36 -0800

    Continuing with the refactor for {bz 4694}, add logic to manage the signal_thread_tag table in trigger.

--- nlw/etc/socialtext/db/socialtext-131-to-132.sql
+++ nlw/etc/socialtext/db/socialtext-131-to-132.sql
@@ -7,6 +7,23 @@ BEGIN
     UPDATE event
        SET hidden = TRUE
      WHERE event.signal_id = NEW.signal_id;
+
+    DELETE FROM signal_thread_tag WHERE signal_id = NEW.signal_id;
+
+    IF NEW.in_reply_to_id IS NOT NULL then
+      DELETE FROM signal_thread_tag where signal_id = NEW.in_reply_to_id;
+
+      INSERT INTO signal_thread_tag (signal_id, tag, user_id)
+        SELECT DISTINCT NEW.in_reply_to_id, lower(tag), user_id
+          FROM signal_tag tag JOIN signal USING (signal_id)
+          WHERE signal.signal_id = NEW.in_reply_to_id AND NOT signal.hidden
+        UNION
+        SELECT DISTINCT NEW.in_reply_to_id, lower(tag), user_id
+          FROM signal_tag tag JOIN signal USING (signal_id)
+          WHERE
+            signal.in_reply_to_id = NEW.in_reply_to_id
+            AND NOT signal.hidden;
+    END IF;
   END IF;
   RETURN NEW;
 END;
