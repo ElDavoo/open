dafd1e9b7c2d8cb68566be7b7b6db45b1ef9664e
Audrey Tang <audrey.tang@socialtext.com>
2010-12-14 23:47:01 -0800

    * Fix {bz: 4761}: Including a page with a Table, HR or .pre fails due to {bz: 4397}'s fix.

--- nlw/share/skin/wikiwyg/javascript/lib/Wikiwyg/Wikitext.js
+++ nlw/share/skin/wikiwyg/javascript/lib/Wikiwyg/Wikitext.js
@@ -1541,7 +1541,7 @@ proto.convert_html_to_wikitext = function(html, isWholeDocument) {
 
         // {bz: 4738}: Don't run _format_one_line on top-level tables, HRs and PREs.
         $(dom).find('td, hr, pre')
-            .parents('span, a, h1, h2, h3, h4, h5, h6, b, strong, i, em, strike, del, s, tt, code, kbd, samp, var, u')
+            .parents('span:not(.nlw_phrase), a, h1, h2, h3, h4, h5, h6, b, strong, i, em, strike, del, s, tt, code, kbd, samp, var, u')
             .addClass('_st_format_div');
 
         $(dom).find('._st_walked').removeClass('_st_walked');
