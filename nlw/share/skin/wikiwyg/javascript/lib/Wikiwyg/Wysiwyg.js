abd84b0aa7be3a41dcb50e5341cfebc3d147d9b8
Audrey Tang <audrey.tang@socialtext.com>
2010-12-29 07:28:28 -0800

    * IE7 uses keydown as well as IE6; IE8 needs keypress.

--- nlw/share/skin/wikiwyg/javascript/lib/Wikiwyg/Wysiwyg.js
+++ nlw/share/skin/wikiwyg/javascript/lib/Wikiwyg/Wysiwyg.js
@@ -666,12 +666,12 @@ proto.enable_table_navigation_bindings = function() {
     if (jQuery.browser.mozilla && navigator.oscpu.match(/Mac/)) {
         event_name = "keypress";
     }
-    if (jQuery.browser.msie && jQuery.browser.version > 6) {
+    if (jQuery.browser.msie && jQuery.browser.version > 7) {
         event_name = "keypress";
     }
 
     self.bind( event_name, function (e) {
-        if (e.metaKey || e.ctrlKey) { return; }
+        if (e.metaKey || e.ctrlKey) { return true; }
         switch (e.keyCode) {
             case 9: { // Tab
                 var $cell = self.find_table_cell_with_cursor();
