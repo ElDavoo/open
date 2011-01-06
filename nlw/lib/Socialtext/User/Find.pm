c0d16236635217726016bcc9b53a8603f4893b99
Audrey Tang <audrey.tang@socialtext.com>
2011-01-05 23:32:23 -0800

    * Amend the fix to {bz: 4811}: The preferred-name match needs to be a prefix   match, not an anywhere match.

--- nlw/lib/Socialtext/User/Find.pm
+++ nlw/lib/Socialtext/User/Find.pm
@@ -201,7 +201,7 @@ sub typeahead_find {
 
             # {bz: 4811}: If preferred name does not match, return first+last name
             # as "display_name" so client-side lookahead can highlight them instead.
-            if (length $prefix and index(lc $_->{display_name}, $prefix) == -1) {
+            if (length $prefix and substr(lc $_->{display_name}, 0, length $prefix) ne $prefix) {
                 $_->{display_name} = $user->proper_name;
             }
 
@@ -222,7 +222,7 @@ sub typeahead_find {
         # {bz: 4811}: If preferred name does not match, return first+last name
         # as "display_name" so client-side lookahead can highlight them instead.
         my $user;
-        if (length $prefix and index(lc $row->{display_name}, $prefix) == -1) {
+        if (length $prefix and substr(lc $row->{display_name}, 0, length $prefix) ne $prefix) {
             $user = Socialtext::User->new(user_id => $row->{user_id});
             $row->{display_name} = $user->proper_name;
         }
