8f9352ba2ee82b7ef8f3c294a61b47b84fc66b06
Luke Closs <luke.closs@socialtext.com>
2011-01-20 14:37:57 -0800

    forgot to add this legacy package, needed for reading old style pages

---
+++ nlw/lib/Socialtext/Page/Legacy.pm
@@ -0,0 +1,98 @@
+# @COPYRIGHT@
+package Socialtext::Page::Legacy;
+use strict;
+use warnings;
+
+use Socialtext::Encode;
+use Socialtext::File;
+use Socialtext::Paths;
+use Socialtext::PageMeta;
+use Socialtext::Search::AbstractFactory;
+use Socialtext::Timer qw/time_scope/;
+use Socialtext::EmailSender::Factory;
+use Socialtext::l10n qw(loc system_locale);
+use Socialtext::WikiText::Parser;
+use Socialtext::WikiText::Emitter::SearchSnippets;
+use Socialtext::String;
+use Socialtext::Events;
+use Socialtext::SQL qw/:exec :txn get_dbh sql_parse_timestamptz/;
+use Socialtext::SQL::Builder qw/sql_insert_many/;
+use Socialtext::Permission 'ST_READ_PERM';
+
+sub parse_headers {
+    my $self = shift;
+    my $headers = shift;
+    my $metadata = {};
+    for (split /\n/, $headers) {
+        next unless /^(\w\S*):\s*(.*)$/;
+        my ($attribute, $value) = ($1, $2);
+        if (defined $metadata->{$attribute}) {
+            $metadata->{$attribute} = [$metadata->{$attribute}]
+              unless ref $metadata->{$attribute};
+            push @{$metadata->{$attribute}}, $value;
+        }
+        else {
+            $metadata->{$attribute} = $value;
+        }
+    }
+
+    # Putting whacky whitespace in a page title can kill javascript on the
+    # front-end. This fixes {bz: 3475}.
+    if ($metadata->{Subject}) {
+        $metadata->{Subject} =~ s/\s/ /g;
+    }
+
+    return $metadata;
+}
+
+sub read_and_decode_file {
+    my $filename       = shift;
+    my $return_content = shift;
+    die "No such file $filename" unless -f $filename;
+    die "File path contains '..', which is not allowed."
+        if $filename =~ /\.\./;
+
+    # Note: avoid using '<:raw' here, it sucks for performance
+    open(my $fh, '<', $filename)
+        or die "Can't open $filename: $!";
+    binmode($fh); # will Encode bytes to characters later
+
+    my $buffer;
+    {
+        # slurp in the header only:
+        local $/ = "\n\n";
+        $buffer = <$fh>;
+    }
+
+    if ($return_content) { 
+        # slurp in the rest of the file:
+        local $/ = undef;
+        $buffer = <$fh> || '';
+    }
+
+    $buffer = Socialtext::Encode::noisy_decode(
+            input => $buffer,
+            blame => $filename
+    );
+
+    $buffer =~ s/\015\012/\n/g;
+    $buffer =~ s/\015/\n/g;
+    return $buffer;
+}
+
+sub revision_file {
+    my $self = shift;
+    my $revision_id = shift;
+    my $filename = join '/', 
+        ( $self->database_directory, $self->id, $revision_id . '.txt' );
+    return $filename;
+}
+
+sub database_directory {
+    my $self = shift;
+    return Socialtext::Paths::page_data_directory(
+        $self->hub->current_workspace->name);
+}
+
+
+1;
