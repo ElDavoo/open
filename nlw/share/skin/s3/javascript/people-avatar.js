e0221940f4de7b4ab2e0aa5091437ef03916c667
Gabe Wachob <gwachob@wachob.com>
2011-01-26 16:35:03 -0800

    Fix for ie7

--- nlw/share/skin/s3/javascript/people-avatar.js
+++ nlw/share/skin/s3/javascript/people-avatar.js
@@ -204,26 +204,29 @@ Avatar.prototype = {
         var sametime_elem_parent = sametime_elem.parent();
         var sametime_sn = sametime_elem.text();
         if (sametime_sn) {
-            $.getScript("http://localhost:59449/stwebapi/getStatus.js",
+            $.getScript("http://localhost:59449/stwebapi/getStatus.js?_="+(new Date().getTime()),
                 function () {
-                    sametime_elem.replaceWith(
-                        jQuery('<a></a>').
-                            css('cursor', 'pointer').
-                            text(sametime_sn).
-                            click(function() {
-                                sametime_invoke('chat', sametime_sn);
-                            })
-                    )
-                    sametime_elem = sametime_elem_parent.find('a');
-                    var sametime_obj = new sametime.livename(sametime_sn, null);
-                    updateStatus = function(contact) {
-                        sametime_elem.before(jQuery("<img></img>").
-                            attr('src', sametime_helper.getStatusImgUrl(contact.status)).
-                            attr('title', contact.statusMessage));
-                        sametime_elem.before(" ");
-                        sametime_elem.attr('title', contact.statusMessage);         
+                    // The following is to make ie7 happy
+                    if (typeof sametime != 'undefined') {
+                        sametime_elem.replaceWith(
+                            jQuery('<a></a>').
+                                css('cursor', 'pointer').
+                                text(sametime_sn).
+                                click(function() {
+                                    sametime_invoke('chat', sametime_sn);
+                                })
+                        )
+                        sametime_elem = sametime_elem_parent.find('a');
+                        var sametime_obj = new sametime.livename(sametime_sn, null);
+                        updateStatus = function(contact) {
+                            sametime_elem.before(jQuery("<img></img>").
+                                attr('src', sametime_helper.getStatusImgUrl(contact.status)).
+                                attr('title', contact.statusMessage));
+                            sametime_elem.before(" ");
+                            sametime_elem.attr('title', contact.statusMessage);         
+                        }
+                        sametime_obj.runActionWithCallback('getstatus', 'updateStatus');
                     }
-                    sametime_obj.runActionWithCallback('getstatus', 'updateStatus');
                 });
         }    
         this.mouseOver();
