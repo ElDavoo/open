21fc9c100532da1e4084558be06482d3cea38681
Audrey Tang <audrey.tang@socialtext.com>
2011-01-28 08:23:02 -0800

    * {widget: rss} now renders in Wiki pages. \o/

--- nlw/lib/Socialtext/WidgetPlugin.pm
+++ nlw/lib/Socialtext/WidgetPlugin.pm
@@ -26,19 +26,69 @@ use Class::Field qw( const );
 use Socialtext::Formatter::Phrase ();
 
 const wafl_id => 'widget';
-const wafl_reference_parse => qr/^\s*([^\s#]+)(?:\s*#\s*(\d+))?\s*$/
+const wafl_reference_parse => qr/^\s*([^\s#]+)(?:\s*#\s*(\d+))?\s*$/;
 
 sub html {
     my $self = shift;
     my ($widget, $serial) = $self->arguments =~ $self->wafl_reference_parse;
 
     $serial ||= 1;
-    $widget = "local:widget:$widget" unless $widget =~ /:/;
+    $widget = "local:widgets:$widget" unless $widget =~ /:/;
 
+    my $page_id = $self->current_page_id;
     my $container = Socialtext::Gadgets::Container::Wafl->Fetch(
         owner => $self->hub->current_workspace, 
         viewer => $self->hub->current_user,
-        name => "$widget#$serial",
+        name => "$widget##$page_id#$serial",
+    );
+
+    return $self->hub->template->process($container->view_template,
+        pluggable => $self->hub->pluggable,
+        container => $container->template_vars,
+    );
+}
+
+################################################################################
+package Socialtext::Gadgets::Container::Wafl;
+use Moose;
+use constant 'type'            => 'wafl';
+use constant 'links_template'  => '';
+use constant 'hello_template'  => '';
+use constant 'footer_template' => '';
+use constant 'header_template' => '';
+use constant 'global'          => 1;
+use constant 'title'           => '';
+use constant 'plugin'          => "widgets";
+use constant 'view_template'   => 'view/container.wafl';
+
+with 'Socialtext::Gadgets::Container';
+has '+owner' => ( isa => 'Socialtext::Workspace' );
+has 'src' => (
+    is => 'ro', isa => 'Str',
+    lazy_build => 1,
+);
+sub _build_src {
+    my $self = shift;
+    my $src = $self->name;
+    $src =~ s{(.*)##.*}{$1};
+    return $src;
+}
+
+sub JoinSQL {''}
+
+sub _build_env {+{}}
+
+sub _build_columns {[
+    { style => 'width: 99%', borderless => 1 },
+]}
+
+sub default_gadgets {
+    my $self = shift;
+    return (
+        {
+            src => $self->src,
+            col => 0, fixed => 1,
+        },
     );
 }
 
