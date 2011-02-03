f748a6f133b7cc6339932ff25d823454313be597
Audrey Tang <audrey.tang@socialtext.com>
2011-02-02 22:01:54 -0800

    * Fix performance problems in Preview->Richtext and Wikitext->Richtext transitions as well. (cherry picked from commit 1896129c8c513ebb665d14ce15fd31dd4d518d2d)

--- nlw/share/skin/wikiwyg/javascript/lib/Wikiwyg/Wysiwyg.js
+++ nlw/share/skin/wikiwyg/javascript/lib/Wikiwyg/Wysiwyg.js
@@ -2188,7 +2188,14 @@ proto.fromHtml = function(html) {
     }
 
     var dom = document.createElement('div');
-    dom.innerHTML = html;
+    dom.innerHTML = html.replace(
+        new RegExp(
+            '(<!--[\\d\\D]*?-->)|(<(span|div)\\sclass="nlw_phrase">)[\\d\\D]*?(<!--\\swiki:\\s[\\d\\D]*?\\s--><\/\\3>)',
+            'g'
+        ), function(_, _1, _2, _3, _4) {
+            return(_1 ? _1 : _2 + _4);
+        }
+    );
     this.sanitize_dom(dom);
     this.set_inner_html(dom.innerHTML);
     this.setWidgetHandlers();
