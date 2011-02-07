9c8f5001a8520e3ce0385bb67f8c37ce981469a6
Stash <jeremy.stashewsky@socialtext.com>
2011-02-07 14:26:18 -0800

    Allow creating users with non-email usernames

--- nlw/lib/Test/Socialtext.pm
+++ nlw/lib/Test/Socialtext.pm
@@ -634,7 +634,9 @@ my @Added_groups;
         my $uniq = delete $opts{unique_id} || create_unique_id;
         my $acct = delete $opts{account} || Socialtext::Account->Default;
 
-        $opts{email_address} = $opts{username} = $uniq.'@ken.socialtext.net';
+        $opts{email_address} = $uniq.'@ken.socialtext.net';
+        $opts{username} = delete $opts{username_isnt_email}
+            ? $uniq : $opts{email_address};
         $opts{created_by_user_id} ||= Socialtext::User->SystemUser->user_id;
 
         my $user = Socialtext::User->create(
