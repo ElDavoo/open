aff47662673edfdacd40fb433a02e762aa076138
Audrey Tang <audrey.tang@socialtext.com>
2011-02-09 09:36:53 -0800

    * {bz: 4906}: Green SC tests; no need to explicitly call Save now.

--- nlw/share/skin/js-test/socialcalc/t/revision.t.js
+++ nlw/share/skin/js-test/socialcalc/t/revision.t.js
@@ -20,7 +20,6 @@ t.runAsync([
         t.is(t.$('#st-spreadsheet-preview td:first').text(), 'test', 'Revision');
         t.ok(t.$('#st-save-revision-button-link'), 'Restore Revision (disabled for new spreadsheets)');
         t.ok(t.$('#st-edit-revision-button-link'), 'Edit Revision (disabled for new spreadsheets)');
-        t.click('#st-save-button-link');
         t.endAsync();
     }
 ]);
