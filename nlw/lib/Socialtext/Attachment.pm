6a39b36b75daf90a135db3891edcfabed207b6b8
Brandon Noard <brandon.noard@socialtext.com>
2011-02-14 12:03:41 -0800

    a ->ensure_stored/copy combo can be replaced with ->copy_to_file

--- nlw/lib/Socialtext/Attachment.pm
+++ nlw/lib/Socialtext/Attachment.pm
@@ -274,9 +274,7 @@ sub extract {
     # extract the archive, so that must be preserved here.
     my $tmpdir = File::Temp::tempdir(CLEANUP => 1);
     my $archive = "$tmpdir/".$self->clean_filename;
-
-    $self->ensure_stored();
-    File::Copy::copy($self->disk_filename, $archive);
+    $self->copy_to_file($archive);
 
     my @files = Socialtext::ArchiveExtractor->extract(archive => $archive);
     # before everything-in-the-db this used to try to add the archive itself
