bf66e228f673d7cffe78adaa79990073c7576dfb
Graham TerMarsch <graham.termarsch@socialtext.com>
2011-02-21 14:15:11 -0800

    Document the new ST:U:Restrictions:base module.

--- nlw/lib/Socialtext/User/Restrictions/base.pm
+++ nlw/lib/Socialtext/User/Restrictions/base.pm
@@ -90,3 +90,94 @@ sub renew {
 }
 
 1;
+
+=head1 NAME
+
+Socialtext::User::Restrictions::base - Base role for User Restrictions
+
+=head1 SYNOPSIS
+
+    my $restriction = Socialtext::User::Restrictions->FetchByToken($token);
+
+    # check if restriction has expired
+    if ($restriction->has_expired) {
+        ...
+    }
+
+    # clear/remove/delete the restriction
+    $restriction->clear;
+
+=head1 DESCRIPTION
+
+This module provides a Moose Role for User Restrictions; restrictions placed
+on a User record that would prevent them from having access to the system
+until some other condition has been met (and the restriction cleared).
+
+=head1 ATTRIBUTES
+
+=over
+
+=item user_id
+
+B<Required.>  Id for the User that this restriction is for.
+
+=item user
+
+Easy accessor to a User object for our C<user_id>.
+
+=item token
+
+A unique sha/hash/token that can be used to uniquely identify this Restriction.
+
+=item expires_at
+
+B<Required.>  Date/time that the restriction expires at.
+
+=item workspace_id
+
+Workspace Id for the Workspace that the User is being invited into.
+
+Yes, this B<is> somewhat out of place here; its present because the original
+implementation allowed for a Workspace Id to be provided when confirming an
+e-mail address so that the User would be auto-added to the Workspace once the
+e-mail address was confirmed.
+
+=item workspace
+
+Easy accessor to a Workspace object for our C<workspace_id>.
+
+=back
+
+=head1 METHODS
+
+=over
+
+=item $class->CreateOrReplace(%params)
+
+Creates (or replaces) an existing Restriction of the derived type, for the
+User.
+
+B<Note,> a User can B<only> have I<one> Restriction of any given Type at any
+time; thus the desire for a quick/easy way to create/replace the existing
+record in the DB with an almost identical one.
+
+=item $restriction->update( { ... } )
+
+Updates the Restriction based on the hash-ref of information provided.
+
+=item $restriction->clear()
+
+Clears the restriction, removing it from the DB.
+
+=item $restriction->has_expired()
+
+Returns true if the Restriction has expired and can not be completed any
+longer, returning false otherwise.
+
+=item $restriction->renew()
+
+Renews the restriction, extending it by the default expration period.
+
+=back
+
+=cut
