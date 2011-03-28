674f40c23cd01188d23be4c9a6051774be1dda8b
Audrey Tang <audrey.tang@socialtext.com>
2011-03-28 10:45:22 -0700

    * Fix French number format in zz locale as per swd++.

--- nlw/lib/Socialtext/l10n/I18N/zz.pm
+++ nlw/lib/Socialtext/l10n/I18N/zz.pm
@@ -7,7 +7,7 @@ use base 'Locale::Maketext';
 sub numf {
     my ($self, $num) = @_;
     while ($num =~ s/^([-+]?\d+)(\d{3})/$1,$2/s) {1}
-    $num =~ tr<.,><,.>;
+    $num =~ tr<.,><, >; # "1,234.56" => "1 234,56"
     return $num;
 }
 
