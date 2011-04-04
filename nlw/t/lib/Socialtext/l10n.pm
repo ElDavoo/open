25a154476a6073981dc7a959780524ab5c2ad573
Audrey Tang <audrey.tang@socialtext.com>
2011-04-04 10:32:32 -0700

    * Fix a few test errors due to "use mocked 'Socialtext::l10n'" not having access to Scalar::Defer.

--- nlw/t/lib/Socialtext/l10n.pm
+++ nlw/t/lib/Socialtext/l10n.pm
@@ -2,21 +2,30 @@
 package Socialtext::l10n;
 use strict;
 use warnings;
-use Scalar::Defer 'defer';
 use base 'Exporter';
 
 our @EXPORT_OK = qw(best_locale system_locale loc loc_lang __);
 our $CUR_LOCALE = 'en';
 our $SYS_LOCALE = 'en';
 
-sub loc {
+sub _reload_l10n {
     delete $INC{'Socialtext/l10n.pm'};
     no warnings 'redefine';
     local $SIG{__WARN__} = sub { 1 };
     require Socialtext::l10n;
     _rebind_overrides();
+    *__ = \&Socialtext::l10n::__;
     *loc = \&Socialtext::l10n::loc;
+}
+
+sub loc {
+    _reload_l10n();
     goto &Socialtext::l10n::loc;
+}
+
+sub __ {
+    _reload_l10n();
+    goto &Socialtext::l10n::__;
 }
 
 sub __loc_lang__ {
@@ -38,10 +47,6 @@ sub _rebind_overrides {
     *loc_lang = \&__loc_lang__;
     *best_locale = \&__best_locale__;
     *system_locale = \&__system_locale__;
-    *__ = sub {
-        my @args = @_;
-        defer { loc(@args) };
-    }
 }
 
 
