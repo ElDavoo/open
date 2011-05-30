425570dfd03fbecd97c6c02e7f987ee034c64c74
Brandon Noard <brandon.noard@socialtext.com>
2011-05-30 10:56:04 -0700

    Export a couple reusable subs; remove overwritten import().
    The import was doing two things:
    1) Making it impossible to export other subs,
    2) Skipping tests if correct infrastructure was not in place.
    
    In fact, we want tests to fail if the infrastructure is missing, lest we
    return a false positive, eg. tests would have failed but we skipped 'em.

--- nlw/lib/Test/Socialtext/Bootstrap/OpenLDAP.pm
+++ nlw/lib/Test/Socialtext/Bootstrap/OpenLDAP.pm
@@ -3,40 +3,67 @@ package Test::Socialtext::Bootstrap::OpenLDAP;
 
 use strict;
 use warnings;
-use base qw(Socialtext::Bootstrap::OpenLDAP);
+use base qw(Exporter Socialtext::Bootstrap::OpenLDAP);
 use Test::Builder;
+use Test::Base;
+use Net::LDAP::Entry;
+use Socialtext::AppConfig;
+use File::Temp qw(tempdir);
+use Socialtext::SQL qw(sql_singlevalue);
 
-sub import {
-    my ($class, @args) = @_;
+our @EXPORT = qw(initialize_ldap set_user_factories 
+                 user_is_unique_to_socialtext);
 
-    # see if failure to auto-detect is fatal or not
-    my $fatal = grep { $_ eq ':fatal' } @args;
+sub initialize_ldap {
+    my $dc = shift;
+    my $dir = shift || tempdir(TMPDIR=>1, CLEANUP=>1); 
 
-    # auto-detect OpenLDAP
-    #
-    # If failure to auto-detect is fatal, abort.  Otherwise, skip all tests
-    # (so we don't impose restrictions on developers that they have to have
-    # OpenLDAP installed in order to run the test suites).
-    eval {
-        $class->_autodetect_slapd();
-        $class->_autodetect_schema_dir();
-        $class->_autodetect_module_dir();
-    };
-    if ($@) {
-        my $message = "OpenLDAP not detected: $@";
-        my $builder = Test::Builder->new();
+    my $dn = "dc=${dc},dc=com";
+    my $ldap = Test::Socialtext::Bootstrap::OpenLDAP->new(
+        base_dn=>$dn,
+        statedir => "$dir/run",
+        datadir => "$dir/data",
+        logfile => "$dir/ldap.log",
+    );
 
-        if ($fatal) {
-            unless ($builder->has_plan()) {
-                $builder->no_plan();
-            }
-            $builder->ok(0);
-            $builder->diag($message);
-            exit;
-        }
+    my $entry = Net::LDAP::Entry->new();
+    $entry->changetype('add');
+    $entry->dn($dn);
+    $entry->add(
+        objectClass => 'dcObject',
+        objectClass => 'organization',
+        dc => $dc,
+        o => "$dc dot com",
+    );
+    my $rc = $ldap->_update(
+        \&Socialtext::Bootstrap::OpenLDAP::_cb_add_entry, [$entry]);
+    ok $rc, "added ldap store '$dc' with base object";
 
-        $builder->skip_all($message);
-    }
+    return $ldap;
+}
+
+sub set_user_factories {
+    my $new_factories = join(';', @_);
+
+    Socialtext::AppConfig->set(user_factories => $new_factories);
+    Socialtext::AppConfig->write();
+
+    my $factories = Socialtext::AppConfig->user_factories();
+    is $factories, $new_factories, "factories are $new_factories";
+
+    return $factories;
+}
+
+sub user_is_unique_to_socialtext {
+    my $username = shift;
+    my $driver_key = shift;
+
+    my $count = sql_singlevalue(qq{
+        SELECT COUNT(*) 
+          FROM users
+         WHERE driver_username = ?
+    }, $username);
+    is $count, 1, "one copy of $username exists";
 }
 
 # Over-ridden method to find the starting port number for LDAP during
