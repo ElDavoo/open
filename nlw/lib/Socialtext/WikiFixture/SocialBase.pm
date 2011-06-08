efd32a0dc84fcaba7cd147676f7b8d9c0292ec73
Kevin Jones <kevin.jones@socialtext.com>
2011-06-08 13:56:16 -0700

    add an option to follow redirects

--- nlw/lib/Socialtext/WikiFixture/SocialBase.pm
+++ nlw/lib/Socialtext/WikiFixture/SocialBase.pm
@@ -1454,6 +1454,15 @@ sub get {
         push @headers, map { split m/\s*=\s*/ } split m/\s*,\s*/, $headers;
     }
     $self->_get($uri, \@headers);
+
+    if ($self->{follow_redirects}) {
+        my $resp = $self->{http}->response;
+        if ($resp->code == 301 or $resp->code == 302) {
+            my $location = $resp->header('Location') || die 'No Location header';
+            diag "Redirect $location";
+            $self->get($location, $accept, $headers);
+        }
+    }
 }
 
 =head2 cond_get ( uri, accept, ims, inm )
