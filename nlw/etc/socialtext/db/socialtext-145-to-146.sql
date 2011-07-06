f4a3934624c50af30c4deb8e0d3fe657e8d2c66f
Brandon Noard <brandon.noard@socialtext.com>
2011-07-06 15:16:02 -0700

    Update system table with correct version number.

--- nlw/etc/socialtext/db/socialtext-145-to-146.sql
+++ nlw/etc/socialtext/db/socialtext-145-to-146.sql
@@ -4,7 +4,7 @@ ALTER TABLE "Account"
     ADD COLUMN pref_blob TEXT NOT NULL DEFAULT ''::text;
 
 UPDATE "System"
-   SET value = '145'
+   SET value = '146'
  WHERE field = 'socialtext-schema-version';
 
 COMMIT;
