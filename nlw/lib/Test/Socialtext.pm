9fda0b2a3a3bc5ae69e5f1a0c8b8904b9b7994c8
Brandon Noard <brandon.noard@socialtext.com>
2011-07-06 11:24:21 -0700

    Wedge in a means for cleaning up LDAP user residue.
    There's definitely a better way to make sure intertest cleanup happens, but
    there's also a lot of other stuff to do, so implement a simple but messy
    solution for now.

--- nlw/lib/Test/Socialtext.pm
+++ nlw/lib/Test/Socialtext.pm
@@ -56,6 +56,7 @@ our @EXPORT = qw(
     create_test_badmin
     create_test_workspace
     create_test_group
+    clear_ldap_users
     SSS
     run_smarter_like
     smarter_like
@@ -553,22 +554,7 @@ sub _teardown_cleanup {
 
     sub _remove_all_but_initial_objects {
         while (my ($key,$obj) = each %Objects) {
-            my $iterator = $obj->{get_iterator}->();
-            if (Test::Socialtext::Environment->instance()->verbose) {
-                Test::More::diag("CLEANUP: removing ${key}s");
-            }
-            while (my $item = $iterator->next()) {
-                # remove all but the initial set of objects that were
-                # created and available at startup.
-                my $id = $obj->{get_id}->($item);
-                next if $Initial{$key}{$id};
-
-                # Delete it
-                if (Test::Socialtext::Environment->instance()->verbose) {
-                    my $identifier = $obj->{identifier}->($item);
-                }
-                $obj->{delete_item}->($item);
-            }
+            _generic_delete($key => $obj);
         }
 
         if (Test::Socialtext::Environment->instance()->verbose) {
@@ -579,6 +565,35 @@ sub _teardown_cleanup {
         sql_txn {
             sql_execute("TRUNCATE note, error, exitstatus, job");
         };
+    }
+
+    sub _generic_delete {
+        my $key = shift;
+        my $obj = shift;
+        my $iterator = $obj->{get_iterator}->();
+        if (Test::Socialtext::Environment->instance()->verbose) {
+            Test::More::diag("CLEANUP: removing ${key}s");
+        }
+        while (my $item = $iterator->next()) {
+            # remove all but the initial set of objects that were
+            # created and available at startup.
+            my $id = $obj->{get_id}->($item);
+            next if $Initial{$key}{$id};
+
+            # Delete it
+            if (Test::Socialtext::Environment->instance()->verbose) {
+                my $identifier = $obj->{identifier}->($item);
+            }
+            $obj->{delete_item}->($item);
+        }
+    }
+
+    sub clear_ldap_users {
+        for my $user (Socialtext::User->All->all) {
+            next if $user->is_system_created;
+            next unless $user->homunculus->driver_name eq 'LDAP';
+            Test::Socialtext::User->delete_recklessly($user);
+        }
     }
 }
 
