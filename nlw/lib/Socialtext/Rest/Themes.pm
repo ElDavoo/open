05cc4c83731d3297ce7534c362dbd1299e176964
Brandon Noard <brandon.noard@socialtext.com>
2011-08-01 08:20:12 -0700

    Make this its own method for re-use later.

--- nlw/lib/Socialtext/Rest/Themes.pm
+++ nlw/lib/Socialtext/Rest/Themes.pm
@@ -22,15 +22,23 @@ sub GET_theme {
     my $rest = shift;
 
     return $self->not_authorized() if $self->rest->user->is_guest;
+    my $theme = $self->_get_theme();
+
+    return $theme
+        ? encode_json($theme->as_hash)
+        : $self->no_resource('theme');
+}
+
+sub _get_theme {
+    my $self = shift;
+
     my $theme;
     $theme = Socialtext::Theme->Load(theme_id => $self->theme)
         if $self->theme =~ /^\d+$/;
 
     $theme ||= Socialtext::Theme->Load(name => $self->theme);
 
-    return $theme
-        ? encode_json($theme->as_hash)
-        : $self->no_resource('theme');
+    return $theme;
 }
 
 __PACKAGE__->meta->make_immutable(inline_constructor => 0);
