e0a1f29e6f402f4ca78985d28a1bc8349716a966
Brandon Noard <brandon.noard@socialtext.com>
2011-08-09 08:50:29 -0700

    Add a handler for working with settings.
    This is still very much a work in progress.

---
+++ nlw/lib/Socialtext/Handler/Settings.pm
@@ -0,0 +1,125 @@
+package Socialtext::Handler::Settings;
+use Moose;
+use Socialtext::Permission qw(ST_ADMIN_WORKSPACE_PERM);
+use Socialtext::l10n qw(loc);
+use namespace::clean -except=>'meta';
+
+with 'Socialtext::Handler::Base';
+extends 'Socialtext::Rest::Entity';
+
+sub if_authorized_to_view {
+    my $self = shift;
+    my $callback = shift;
+
+    return $self->not_authenticated if $self->rest->user->is_guest;
+    return $callback->();
+}
+
+sub if_authorized_to_edit {
+    my $self = shift;
+    my $callback = shift;
+    return $self->if_authorized_to_view($callback);
+}
+
+sub get_html {
+    my $self = shift;
+    my $rest = shift;
+
+    my $vars = $self->_settings_vars();
+    $vars->{section} = 'global';
+
+    $self->rest->header('Content-Type' => 'text/html; charset=utf-8');
+    return $self->render_template('view/settings', $vars);
+}
+
+around 'GET_space' => \&wrap_get;
+sub GET_space { 
+    my $self = shift;
+    my $rest = shift;
+
+    my $vars = $self->_settings_vars();
+    $vars->{section} = 'space';
+
+    $self->rest->header('Content-Type' => 'text/html; charset=utf-8');
+    return $self->render_template('view/settings', $vars);
+}
+
+around 'GET_create' => \&wrap_get;
+sub GET_create {
+    my $self = shift;
+    my $rest = shift;
+
+    my $vars = $self->_settings_vars();
+    $vars->{section} = 'create';
+
+    $self->rest->header('Content-Type' => 'text/html; charset=utf-8');
+    return $self->render_template('view/settings', $vars);
+}
+
+sub _settings_vars {
+    my $self = shift;
+
+    my $id = eval { $self->workspace_id };
+    my $cursor = $self->rest->user->workspaces;
+    my @spaces = ();
+    my $i = 0;
+
+    my $vars = {};
+    while (my $space = $cursor->next()) {
+        $vars->{active_ix} = $i if $id && $space->workspace_id == $id;
+        push @spaces, {
+            title => $space->title,
+            id => $space->workspace_id,
+            prefs => $self->_space_prefs($space),
+            active => $id && $space->workspace_id == $id ? 1 : 0,
+        };
+        $i++;
+    }
+    $vars->{spaces} = \@spaces;
+
+    return $vars;
+}
+
+sub _space_prefs {
+    my $self = shift;
+    my $space = shift;
+
+    my $pref = eval { $self->pref };
+    my $id = eval { $self->workspace_id };
+
+    my $is_space = $id && $space->workspace_id == $id;
+    my $can_admin = $space->user_can(
+        user=>$self->rest->user,
+        permission=>ST_ADMIN_WORKSPACE_PERM,
+    );
+
+    my %abilities = (
+        preferences => loc('Preferences'),
+        blog => loc('Create Blog'),
+        unsubscribe => loc('Unsubscribe'),
+    );
+
+    if ($can_admin) {
+        %abilities = (
+            %abilities,
+            manage => loc('Manage All Users'),
+            invite => loc('Invite New Users'),
+            features => loc('Workspace Features'),
+        );
+    }
+
+    my $prefs = [];
+    for my $ability (keys %abilities) {
+        my $is_ability = $pref && $ability eq $pref;
+        push @$prefs, +{
+            name => $ability,
+            title => $abilities{$ability},
+            active => $is_space && $is_ability ? 1 : 0,
+        };
+    }
+
+    return $prefs;
+}
+
+__PACKAGE__->meta->make_immutable(inline_constructor => 0);
+1;
