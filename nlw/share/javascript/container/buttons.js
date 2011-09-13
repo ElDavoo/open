1be58a96cbc2575a6f79cd3359c1bde9473c72e8
Kevin Jones <kevin.jones@socialtext.com>
2011-09-13 13:29:02 -0700

    Move person buttons to buttons.js, and include Person.js and Avatar.js in socialtext-api

---
+++ nlw/share/javascript/container/buttons.js
@@ -0,0 +1,51 @@
+gadgets.container.bindButtons = function() {
+    $('#st-add-widget').click(function() {
+        socialtext.dialog.show('opensocial-gallery', {
+            view: gadgets.container.view,
+            account_id: gadgets.container.viewer.primary_account_id || 0
+        });
+        return false;
+    });
+
+    $('#st-edit-layout').click(function() {
+        gadgets.container.enterEditMode();
+        return false;
+    });
+
+    $('#st-save-layout').click(function() {
+        socialtext.dialog.show('save-layout');
+        return false;
+    });
+    $('#st-cancel-layout').click(function() {
+        self._in_edit_mode = false;
+        gadgets.container.loadLayout(gadgets.container.base_url, function() {
+            gadgets.container.leaveEditMode();
+        });
+        return false;
+    });
+    $('#st-revert-layout').click(function() {
+        gadgets.container.loadDefaults();
+        return false;
+    });
+
+    if ($('#st-watchperson-indicator').size()) {
+        var $indicator = $(this);
+        function updateNetworksWidget() {
+            try {
+                gadgets.rpc.call('..', 'pubsub', null, 'publish', 'update');
+            } catch (e) {}
+        }
+
+        var person = new Person({
+            id: gadgets.container.owner.user_id,
+            best_full_name: gadgets.container.owner.name,
+            self: false,
+            onFollow: updateNetworksWidget,
+            onStopFollowing: updateNetworksWidget
+        });
+        person.loadWatchlist(function() {
+            console.log('creating indicator');
+            person.createFollowLink($indicator);
+        });
+    }
+};
