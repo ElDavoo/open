a2e48d0bd77ca5dd571e786762da339df3e76fb5
Audrey Tang <audrey.tang@socialtext.com>
2011-09-21 19:21:40 -0700

    * Fix {bz: 5491}: wiki2html/html2wiki should work with empty strings. (cherry picked from commit c6c19e3df37df7fcd02c8a7119556029c194eb80)

--- nlw/lib/Socialtext/Client/Wiki.pm
+++ nlw/lib/Socialtext/Client/Wiki.pm
@@ -2,6 +2,7 @@ package Socialtext::Client::Wiki;
 # @COPYRIGHT@
 use 5.12.0;
 use warnings;
+no warnings 'uninitialized';
 use parent 'Exporter';
 use LWP::UserAgent;
 use HTTP::Request::Common;
@@ -10,11 +11,13 @@ use Socialtext::HTTP::Ports;
 our @EXPORT = qw( wiki2html html2wiki );
 
 sub wiki2html {
+    return '' unless length $_[0];
     unshift @_, 'wiki';
     goto &_request;
 }
 
 sub html2wiki {
+    return '' unless length $_[0];
     unshift @_, 'html';
     goto &_request;
 }
