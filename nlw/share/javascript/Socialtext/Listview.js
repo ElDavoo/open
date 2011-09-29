2f9fdb68a85305d0b70ce113b3e4a0e2eda962e3
Kevin Jones <kevin.jones@socialtext.com>
2011-09-29 10:55:08 -0700

    refactor listview templates in order to fix an extraneous closing div tag

---
+++ nlw/share/javascript/Socialtext/Listview.js
@@ -0,0 +1,42 @@
+Socialtext.prototype.setupListview = (function($){
+    return function(query_start) {
+        $('#sort-picker').live('change', function() {
+            var selected = jQuery('select#sort-picker').val();
+            window.location = query_start + ';' + selected;
+        });
+
+        $('#st-listview-submit-pdfexport').live('click', function() {
+            if (!$('.st-listview-selectpage-checkbox:checked').size()) {
+                alert(loc("error.no-page-pdf"));
+            }
+            else {
+                $('#st-listview-action').val('pdf_export')
+                $('#st-listview-filename').val(Socialtext.wiki_id + '.pdf');
+                $('#st-listview-form').submit();
+            }
+            return false;
+        });
+
+        $('#st-listview-submit-rtfexport').live('click', function() {
+            if (!$('.st-listview-selectpage-checkbox:checked').size()) {
+                alert(loc("error.no-page-doc"));
+            }
+            else {
+                $('#st-listview-action').val('rtf_export')
+                $('#st-listview-filename').val(Socialtext.wiki_id + '.rtf');
+                $('#st-listview-form').submit();
+            }
+            return false;
+        });
+
+        $('#st-listview-selectall').live('click', function () {
+            var self = this;
+            $('input[type=checkbox]').each(function() {
+                if ( ! $(this).attr('disabled') ) {
+                    $(this).attr('checked', self.checked);
+                }
+            });
+            return true;
+        });
+    };
+})(jQuery);
