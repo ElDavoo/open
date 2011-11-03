60bc1bb87df0c247a89c6aa419e16d1b6063429e
Audrey Tang <audrey.tang@socialtext.com>
2011-11-03 14:31:56 +0800

    * Remove console.log

--- nlw/share/javascript/activities/editor.js
+++ nlw/share/javascript/activities/editor.js
@@ -462,7 +462,6 @@ $.extend(Activities.Editor.prototype, {
     },
 
     showProfileMention: function() {
-        console.log(this.viewer_id);
         this.startMention({
             user_id: this.mention_user_id,
             best_full_name: this.mention_user_name
