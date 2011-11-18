0f1de71766b45a2d7763374f5a9d621a879cffe4
Brandon Noard <brandon.noard@socialtext.com>
2011-11-18 13:16:12 -0800

    Don't tidy uploads until we've added references to Account Theme Attachments.
    This will get fixed in a patch release in the very near future.

--- nlw/lib/Socialtext/Job/TidyUploads.pm
+++ nlw/lib/Socialtext/Job/TidyUploads.pm
@@ -13,36 +13,37 @@ use constant AT_A_TIME => 100; # just to limit the run-time of the select
 sub do_work {
     my $self = shift;
 
-    my @references = map {
-          "attachment_id IN (SELECT attachment_id FROM $_)"
-    } Socialtext::Upload::TABLE_REFS;
-
-
-    push @references, map { "attachment_id IN (SELECT ". $_ ."_id FROM theme)" }
-        @Socialtext::Theme::UPLOADS; 
-
-    my $referenced = join "\nOR\n", @references;
-
-    my $sth = sql_execute(qq{
-        SELECT attachment_id FROM attachment a
-        WHERE NOT ( $referenced )
-        LIMIT ?
-    }, AT_A_TIME);
-
-    return $self->completed if $sth->rows == 0;
-
-    while (my $row = $sth->fetchrow_arrayref) {
-        my $att_id = $row->[0];
-        # run in a txn to avoid conflicts with things picking abandoned rows
-        sql_txn {
-            sql_execute(q{
-                DELETE FROM attachment WHERE attachment_id = ?
-            }, $att_id)
-        };
-    }
-
-    # Now clean up temp attachments
-    Socialtext::Upload->CleanTemps();
+# Uncomment this code when theme attachment bug is fixed.
+#     my @references = map {
+#           "attachment_id IN (SELECT attachment_id FROM $_)"
+#     } Socialtext::Upload::TABLE_REFS;
+# 
+# 
+#     push @references, map { "attachment_id IN (SELECT ". $_ ."_id FROM theme)" }
+#         @Socialtext::Theme::UPLOADS; 
+# 
+#     my $referenced = join "\nOR\n", @references;
+# 
+#     my $sth = sql_execute(qq{
+#         SELECT attachment_id FROM attachment a
+#         WHERE NOT ( $referenced )
+#         LIMIT ?
+#     }, AT_A_TIME);
+# 
+#     return $self->completed if $sth->rows == 0;
+# 
+#     while (my $row = $sth->fetchrow_arrayref) {
+#         my $att_id = $row->[0];
+#         # run in a txn to avoid conflicts with things picking abandoned rows
+#         sql_txn {
+#             sql_execute(q{
+#                 DELETE FROM attachment WHERE attachment_id = ?
+#             }, $att_id)
+#         };
+#     }
+# 
+#     # Now clean up temp attachments
+#     Socialtext::Upload->CleanTemps();
 
     # Check again in FREQUENCY seconds.  We'll stop when there's no more work
     # to do.
